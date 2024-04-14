a79fc4e2266cc84b9fca46bfe101fbe066a7b0af
Duc Tri Nguyen <dnguye69@gmu.edu>
2024-04-14 15:40:01 -0400

    forward output to pipe directly
    Signed-off-by: Duc Tri Nguyen <dnguye69@gmu.edu>

--- checksum.sh
+++ checksum.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 # SPDX-License-Identifier: Apache-2.0
+set -o xtrace
 
 # This script executes a binary file, captures its output, then generates and compares its SHA-256 hash with a provided one.
-output=$(./$1)
 
-output_hash=$(echo "$output" | sha256sum | awk '{ print $1 }')
+output_hash=$(./$1 | sha256sum | awk '{ print $4 }')
 
-if [ "$output_hash" == "$2" ]; then
-    echo "$1 Hashes match."
-    exit 0
+if [[ ${output_hash} == "${2}" ]]; then
+	echo "${1} Hashes match."
+	exit 0
 else
-    echo "$1 Hashes do not match: $output_hash vs $2"
-    exit 1
+	echo "${1} Hashes do not match: ${output_hash} vs ${2}"
+	exit 1
 fi
