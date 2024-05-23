cf080306c3c78954a55b2f9b03e0276cd9a8894a
Matthias J. Kannwischer <matthias@kannwischer.eu>
2024-05-23 11:25:40 +0800

    clean up flake.nix
    Signed-off-by: Matthias J. Kannwischer <matthias@kannwischer.eu>

--- flake.nix
+++ flake.nix
@@ -21,6 +21,8 @@
           core = with pkgs; [
             # formatter & linters
             astyle # 3.4.10
+            nixpkgs-fmt
+            shfmt
           ];
         in
         {
@@ -28,11 +30,6 @@
             packages = core ++ [
               direnv
               nix-direnv
-
-              # formatter & linters
-              nixpkgs-fmt
-              shfmt
-              codespell
             ];
 
             shellHook = ''
