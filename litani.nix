e49e379e3f6bfdaf8c3ad10fbb30c9c1a770d372
Matthias J. Kannwischer <matthias@kannwischer.eu>
2024-06-12 19:10:25 +0800

    Add CBMC as dependency to nix (#54)
    * add cbmc/litani/cbmc-viewer to nix
    
    Signed-off-by: Matthias J. Kannwischer <matthias@kannwischer.eu>
    
    * remove native dependency from CI
    
    Signed-off-by: Matthias J. Kannwischer <matthias@kannwischer.eu>
    
    * update to nixos-24.05 channel
    
    Signed-off-by: Matthias J. Kannwischer <matthias@kannwischer.eu>
    
    * fix permission error on trying to overwrite read-only template files
    
    Signed-off-by: Matthias J. Kannwischer <matthias@kannwischer.eu>
    
    * switch to older astyle version
    
    Signed-off-by: Matthias J. Kannwischer <matthias@kannwischer.eu>
    
    * format
    
    Signed-off-by: Matthias J. Kannwischer <matthias@kannwischer.eu>
    
    * clean up litani build
    
    Signed-off-by: Matthias J. Kannwischer <matthias@kannwischer.eu>
    
    * correct cbmc version
    
    Signed-off-by: Matthias J. Kannwischer <matthias@kannwischer.eu>
    
    * fix litani again
    
    Signed-off-by: Matthias J. Kannwischer <matthias@kannwischer.eu>
    
    * build cbmc from source
    
    Signed-off-by: Matthias J. Kannwischer <matthias@kannwischer.eu>
    
    * Revert "build cbmc from source"
    
    This reverts commit 0d5df640598b9085c2a26fe329dceb50d22c2f46.
    
    Signed-off-by: Matthias J. Kannwischer <matthias@kannwischer.eu>
    
    ---------
    
    Signed-off-by: Matthias J. Kannwischer <matthias@kannwischer.eu>

---
+++ litani.nix
@@ -0,0 +1,30 @@
+# SPDX-License-Identifier: Apache-2.0
+
+{ stdenvNoCC
+, fetchFromGitHub
+}:
+
+stdenvNoCC.mkDerivation rec {
+  pname = "litani";
+  version = "8002c240ef4f424039ed3cc32e076c0234d01768";
+  src = fetchFromGitHub {
+    owner = "awslabs";
+    repo = "aws-build-accumulator";
+    rev = "8002c240ef4f424039ed3cc32e076c0234d01768";
+    sha256 = "sha256-UwF/B6lpsjpQn8SW+tCfOXTp14pNBr2sRGujJH3iPLk=";
+  };
+  dontConfigure = true;
+  installPhase = ''
+    mkdir -p $out/bin
+    cp litani $out/bin
+    cp -r lib $out/bin
+    cp -r templates $out/bin
+  '';
+  dontStrip = true;
+  noAuditTmpdir = true;
+
+  meta = {
+    description = "Litani metabuild system";
+    homepage = "https://awslabs.github.io/aws-build-accumulator/";
+  };
+}
