729ccbf60411a61dae677782a68789f9b4c0f5e2
Rod Chapman <rodchap@amazon.com>
2024-06-13 17:04:16 +0100

    Disable auto-formatting in quantified loop invariant
    Signed-off-by: Rod Chapman <rodchap@amazon.com>

--- mlkem/poly.c
+++ mlkem/poly.c
@@ -137,13 +137,15 @@ void poly_compress(uint8_t r[KYBER_POLYCOMPRESSEDBYTES], const poly *a)
         __CPROVER_assigns(i, u, __CPROVER_object_whole(t), __CPROVER_object_whole(r))
         __CPROVER_loop_invariant(i <= KYBER_N) {
         for (size_t j = 0; j < 8; j++)
+// *INDENT-OFF*
             __CPROVER_assigns(j, u, __CPROVER_object_whole(t))
             __CPROVER_loop_invariant(i <= KYBER_N)
             __CPROVER_loop_invariant(j <= 8)
-            __CPROVER_loop_invariant(__CPROVER_forall { size_t k; (0 <= k && k < j) == > (t[k] >= 0 && t[k] < 16) }) {
+            __CPROVER_loop_invariant(__CPROVER_forall { size_t k; (0 <= k && k < j) ==> (t[k] >= 0 && t[k] < 16) }) {
+// *INDENT-ON*
             // map to positive standard representatives
             // REF-CHANGE: Hoist signed-to-unsigned conversion into separate function
-            u = (int32_t) coeff_signed_to_unsigned(a->coeffs[8 * i + j]);
+            { u = (int32_t) coeff_signed_to_unsigned(a->coeffs[8 * i + j]); }
             // REF-CHANGE: Hoist scalar compression into separate function
             t[j] = scalar_compress_q_16(u);
         }
