4aa85e0ca888e3c4f87fb4681a8134ac4bac35e2
Rod Chapman <rodchap@amazon.com>
2024-06-21 09:32:42 +0100

    Increased object-bits setting for this proof.
    Signed-off-by: Rod Chapman <rodchap@amazon.com>

--- cbmc/proofs/poly_compress/desktop.mk
+++ cbmc/proofs/poly_compress/desktop.mk
@@ -15,7 +15,7 @@ pqcrystals_kyber768_ref_poly_compress: $(FILE)_contracts.goto results_smt
 
 
 results_smt: $(FILE)_contracts.goto
-	cbmc --verbosity 6 $(CHECKS) --smt2 $<
+	cbmc --verbosity 6 --object-bits 10 $(CHECKS) --smt2 $<
 
 $(FILE)_contracts.goto: $(FILE).goto
 	goto-instrument --dfcc harness --apply-loop-contracts --enforce-contract $(TARGET) --replace-call-with-contract pqcrystals_kyber768_ref_scalar_compress_q_16 --replace-call-with-contract pqcrystals_kyber768_ref_scalar_compress_q_32 --replace-call-with-contract coeff_signed_to_unsigned $< $@
