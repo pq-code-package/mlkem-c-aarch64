a3d00c7b6721f55ac1fff34bfa085c0e585a5e9e
Lim, Thing-han <15379156+potsrevennil@users.noreply.github.com>
2024-06-28 16:10:43 +0800

    Add support for static linking of glibc on aarch64 and x86_64 linux  (#82)
    * use cross compiled nix gcc for x86_64 instead
    
    Signed-off-by: Thing-han, Lim <15379156+potsrevennil@users.noreply.github.com>
    
    * cleanup and refactor nix
    
    Signed-off-by: Thing-han, Lim <15379156+potsrevennil@users.noreply.github.com>
    
    * fix dynamic linking glibc
    
    Signed-off-by: Thing-han, Lim <15379156+potsrevennil@users.noreply.github.com>
    
    * remove redundant empty lines
    
    Signed-off-by: Thing-han, Lim <15379156+potsrevennil@users.noreply.github.com>
    
    * use specific gcc for the shell
    
    Signed-off-by: Thing-han, Lim <15379156+potsrevennil@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Thing-han, Lim <15379156+potsrevennil@users.noreply.github.com>

--- flake.nix
+++ flake.nix
@@ -16,36 +16,31 @@
     flake-parts.lib.mkFlake { inherit inputs; } {
       imports = [ ];
       systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
-      perSystem = { pkgs, system, ... }:
+      perSystem = { pkgs, ... }:
         let
-          litani = pkgs.callPackage ./litani.nix { };
-          cbmc-viewer = pkgs.callPackage ./cbmc-viewer.nix { };
-          astyle = pkgs.astyle.overrideAttrs (old: rec {
-            version = "3.4.13";
-            src = pkgs.fetchurl {
-              url = "mirror://sourceforge/${old.pname}/${old.pname}-${version}.tar.bz2";
-              hash = "sha256-eKYQq9OelOD5E+nuXNoehbtizWM1U97LngDT2SAQGc4=";
-            };
-          });
-          cbmc = pkgs.cbmc.overrideAttrs (old: rec {
-            version = "a8b8f0fd2ad2166d71ccce97dd6925198a018144";
-            src = pkgs.fetchFromGitHub {
-              owner = "diffblue";
-              repo = old.pname;
-              rev = "${version}";
-              hash = "sha256-mPRkkKN7Hz9Qi6a3fEwVFh7a9OaBFcksNw9qwNOarao=";
-            };
-          });
-
           cbmcpkg = builtins.attrValues
             {
-              cbmc = cbmc;
-              litani = litani; # 1.29.0
-              cbmc-viewer = cbmc-viewer; # 3.8
+              cbmc = pkgs.cbmc.overrideAttrs (old: rec {
+                version = "a8b8f0fd2ad2166d71ccce97dd6925198a018144";
+                src = pkgs.fetchFromGitHub {
+                  owner = "diffblue";
+                  repo = old.pname;
+                  rev = "${version}";
+                  hash = "sha256-mPRkkKN7Hz9Qi6a3fEwVFh7a9OaBFcksNw9qwNOarao=";
+                };
+              }); # 6.0.0
+              litani = pkgs.callPackage ./litani.nix { }; # 1.29.0
+              cbmc-viewer = pkgs.callPackage ./cbmc-viewer.nix { }; # 3.8
             };
 
           linters = builtins.attrValues {
-            astyle = astyle;
+            astyle = pkgs.astyle.overrideAttrs (old: rec {
+              version = "3.4.13";
+              src = pkgs.fetchurl {
+                url = "mirror://sourceforge/${old.pname}/${old.pname}-${version}.tar.bz2";
+                hash = "sha256-eKYQq9OelOD5E+nuXNoehbtizWM1U97LngDT2SAQGc4=";
+              };
+            });
 
             inherit (pkgs)
               nixpkgs-fmt
@@ -55,8 +50,23 @@
               black;
           };
 
-          core = builtins.attrValues
-            {
+          core =
+            let
+              # for x86_64 machine, cross compiled gcc needed to be wrapped with glibc this way for static compilation
+              cross-gcc = with pkgs; wrapCCWith {
+                cc = callPackage ./arm-gnu-gcc.nix { };
+                bintools = with pkgsCross.aarch64-multiplatform; wrapBintoolsWith {
+                  bintools = binutils-unwrapped;
+                  libc = glibc.static;
+                };
+              };
+              aarch64-gcc =
+                if pkgs.stdenv.isx86_64
+                then [ cross-gcc ]
+                else [ (pkgs.gcc13.override { propagateDoc = true; isGNU = true; }) pkgs.glibc pkgs.glibc.static ];
+            in
+            pkgs.lib.optionals pkgs.stdenv.isLinux aarch64-gcc ++
+            builtins.attrValues {
               inherit (pkgs)
                 yq
                 ninja# 1.11.1
@@ -66,49 +76,28 @@
               inherit (pkgs.python3Packages)
                 python
                 click;
-            }
-          ++ {
-            "x86_64-linux" = [ (pkgs.callPackage ./arm-gnu-gcc.nix { }) ];
-            "aarch64-linux" = [ (pkgs.gcc13.override { propagateDoc = true; isGNU = true; }) ];
-            "aarch64-darwin" = [ ];
-            "x86_64-darwin" = [ ];
-          }.${system};
-
-        in
-        {
-          devShells.default = pkgs.mkShellNoCC {
-            packages = core ++ linters ++ cbmcpkg ++ builtins.attrValues {
-              inherit (pkgs)
-                direnv
-                nix-direnv;
             };
 
-            shellHook = ''
-              export PATH=$PWD/scripts:$PWD/scripts/ci:$PATH
-            '';
+          wrapShell = mkShell: attrs:
+            mkShell (attrs // {
+              shellHook = ''
+                export PATH=$PWD/scripts:$PWD/scripts/ci:$PATH
+              '';
+            });
+        in
+        {
+          devShells.default = wrapShell pkgs.mkShellNoCC {
+            packages = core ++ linters ++ cbmcpkg ++
+              builtins.attrValues {
+                inherit (pkgs)
+                  direnv
+                  nix-direnv;
+              };
           };
 
-          devShells.ci = pkgs.mkShellNoCC {
-            packages = core;
-            shellHook = ''
-              export PATH=$PWD/scripts:$PWD/scripts/ci:$PATH
-            '';
-          };
-
-          devShells.ci-linter = pkgs.mkShellNoCC {
-            packages = linters;
-            shellHook = ''
-              export PATH=$PWD/scripts:$PWD/scripts/ci:$PATH
-            '';
-          };
-
-          devShells.ci-cbmc = pkgs.mkShellNoCC {
-            packages = core ++ cbmcpkg;
-            shellHook = ''
-              export PATH=$PWD/scripts:$PWD/scripts/ci:$PATH
-            '';
-          };
-
+          devShells.ci = wrapShell pkgs.mkShellNoCC { packages = core; };
+          devShells.ci-cbmc = wrapShell pkgs.mkShellNoCC { packages = (core ++ cbmcpkg); };
+          devShells.ci-linter = wrapShell pkgs.mkShellNoCC { packages = linters; };
         };
       flake = {
         # The usual flake attributes can be defined here, including system-
