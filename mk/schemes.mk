92dbcbf69d87f7983f61176e786995c02bfe2f98
Thing-han, Lim <15379156+potsrevennil@users.noreply.github.com>
2024-08-26 13:09:37 +0800

    refactor build system objs macro
    Signed-off-by: Thing-han, Lim <15379156+potsrevennil@users.noreply.github.com>

--- mk/schemes.mk
+++ mk/schemes.mk
@@ -1,6 +1,5 @@
 # SPDX-License-Identifier: Apache-2.0
 SOURCES = $(wildcard mlkem/*.c)
-OBJS = $(SOURCES:%.c=$(1)/%.o)
 
 CPPFLAGS += -Imlkem
 TESTS = test_kyber bench_kyber gen_NISTKAT gen_KAT
@@ -10,10 +9,10 @@ MLKEM768_DIR = $(BUILD_DIR)/mlkem768
 MLKEM1024_DIR = $(BUILD_DIR)/mlkem1024
 
 $(MLKEM512_DIR)/bin/%: CPPFLAGS += -DKYBER_K=2
-$(TESTS:%=$(MLKEM512_DIR)/bin/%):$(MLKEM512_DIR)/bin/%: $(MLKEM512_DIR)/test/%.o $(call OBJS,$(MLKEM512_DIR))
+$(TESTS:%=$(MLKEM512_DIR)/bin/%):$(MLKEM512_DIR)/bin/%: $(MLKEM512_DIR)/test/%.c.o $(call MAKE_OBJS,$(MLKEM512_DIR),$(SOURCES))
 
 $(MLKEM768_DIR)/bin/%: CPPFLAGS += -DKYBER_K=3
-$(TESTS:%=$(MLKEM768_DIR)/bin/%):$(MLKEM768_DIR)/bin/%: $(MLKEM768_DIR)/test/%.o $(call OBJS,$(MLKEM768_DIR))
+$(TESTS:%=$(MLKEM768_DIR)/bin/%):$(MLKEM768_DIR)/bin/%: $(MLKEM768_DIR)/test/%.c.o $(call MAKE_OBJS,$(MLKEM768_DIR),$(SOURCES))
 
 $(MLKEM1024_DIR)/bin/%: CPPFLAGS += -DKYBER_K=4
-$(TESTS:%=$(MLKEM1024_DIR)/bin/%):$(MLKEM1024_DIR)/bin/%: $(MLKEM1024_DIR)/test/%.o  $(call OBJS,$(MLKEM1024_DIR))
+$(TESTS:%=$(MLKEM1024_DIR)/bin/%):$(MLKEM1024_DIR)/bin/%: $(MLKEM1024_DIR)/test/%.c.o  $(call MAKE_OBJS,$(MLKEM1024_DIR),$(SOURCES))
