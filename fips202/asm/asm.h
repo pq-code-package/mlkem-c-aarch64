fc539c38aac20e897922e6f3f4db0f24559349ac
Hanno Becker <beckphan@amazon.co.uk>
2024-09-19 08:03:56 +0100

    Add hybrid Neon/Neon Keccak-f1600, don't use scalar ASM on Cortex-A72 (#139)
    * FIPS202: Only pick lazy-rotation assembly for v8.2-A+
    
    Signed-off-by: Hanno Becker <beckphan@amazon.co.uk>
    
    * Add hybrid Neon/Neon Keccak-f1600 ASM
    
    Signed-off-by: Hanno Becker <beckphan@amazon.co.uk>
    
    * Add SHA3 extension flag to Graviton3 build
    
    Signed-off-by: Hanno Becker <beckphan@amazon.co.uk>
    
    * Don't try to detect slow barrel shifter
    
    The `major*100 + minor` semantics of `__ARM_ARCH`
    is not well-supported it seems.
    
    Signed-off-by: Hanno Becker <beckphan@amazon.co.uk>
    
    ---------
    
    Signed-off-by: Hanno Becker <beckphan@amazon.co.uk>

--- fips202/asm/asm.h
+++ fips202/asm/asm.h
@@ -9,45 +9,47 @@
 #ifdef MLKEM_USE_AARCH64_ASM
 void keccak_f1600_x1_scalar_slothy_opt_a55(uint64_t *state);
 void keccak_f1600_x2_v84a_asm_clean(uint64_t *state);
-
-#define keccak_f1600_x1_asm keccak_f1600_x1_scalar_slothy_opt_a55
-#define keccak_f1600_x2_asm keccak_f1600_x2_v84a_asm_clean
+void keccak_f1600_x2_v8a_v84a_asm_hybrid(uint64_t *state);
 
 /*
- * Logic to decide which implementation to use
+ * Logic to decide which implementation to use.
+ *
+ * Source of implementations:
+ * [1]: Hybrid scalar/vector implementations of Keccak and SPHINCS+ on AArch64
+ *      https://eprint.iacr.org/2022/1243.
  */
 
 #if !defined(MLKEM_USE_FIPS202_ASM_FORCE)
 
-// TODO: Allow to overwrite this logic through CFLAGS
+// By default, we use lazy-rotation scalar assembly from [1].
+// NOTE: Lazy rotation is only beneficial for fast Barrel shifters.
+// For CPUs where barrel shifting is slow, like Cortex-A72, this
+// implementation should be disabled by setting SYS_AARCH64_SLOW_BARREL_SHIFTER.
+#if !defined(SYS_AARCH64_SLOW_BARREL_SHIFTER)
+#define MLKEM_USE_FIPS202_X1_ASM
+#define keccak_f1600_x1_asm keccak_f1600_x1_scalar_slothy_opt_a55
+#endif /* !SYS_AARCH64_SLOW_BARREL_SHIFTER */
 
-// If v8.4-A is implemented, use basic implementation using SHA3 instructions
-// from [1].
+// If v8.4-A is implemented, leverage SHA3 instructions.
 //
-// [1]: Hybrid scalar/vector implementations of Keccak and SPHINCS+ on AArch64
-//      https://eprint.iacr.org/2022/1243.
-//
-// TODO: This needs replacing with a scalar/vector hybrid on non-Apple
-// systems pre Cortex-X4, as those don't have enough SIMD units implementing
-// SHA3 instructions to make them worthwhile on their own.
+// The optimal implementation is highly CPU-specific; see [1].
 #if defined(__ARM_FEATURE_SHA3)
-
-#define MLKEM_USE_FIPS202_X1_ASM
+// For Apple-M cores, we use a plain implementation leveraging SHA3
+// instructions only.
+#if defined(__APPLE__)
 #define MLKEM_USE_FIPS202_X2_ASM
+#define keccak_f1600_x2_asm keccak_f1600_x2_v84a_asm_clean
+#else /* __APPLE__ */
+// Non Apple-M cores (pre Cortex-X4 at least) tend to implement SHA3
+// instructions on a subset of Neon units only, in which case a hybrid
+// Neon implementation is needed.
+#define MLKEM_USE_FIPS202_X2_ASM
+#define keccak_f1600_x2_asm keccak_f1600_x2_v8a_v84a_asm_hybrid
+#endif /* __APPLE__ */
 
-// Add this if the assembly implementation expects interleaved lanes
-// #define MLKEM_USE_FIPS202_X2_ASM_ZIPPED
-
-#elif 1
-// TODO: This should exclude CPUs with faster Barrel shift,
-//       notably excluding Cortex-A72.
-
-#define MLKEM_USE_FIPS202_X1_ASM
-
-#endif /* !__ARM_FEATURE_SHA3 */
+#endif /* __ARM_FEATURE_SHA3 */
 
 #endif /* !MLKEM_USE_FIPS202_ASM_FORCE */
-
 #endif /* MLKEM_USE_AARCH64_ASM */
 
 #endif
