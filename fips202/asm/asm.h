284f002b0ef5935e4be3fdc973b7abad1d5f65f3
Hanno Becker <beckphan@amazon.co.uk>
2024-09-19 05:34:57 +0100

    FIPS202: Only pick lazy-rotation assembly for v8.2-A+
    Signed-off-by: Hanno Becker <beckphan@amazon.co.uk>

--- fips202/asm/asm.h
+++ fips202/asm/asm.h
@@ -27,10 +27,12 @@ void keccak_f1600_x2_v84a_asm_clean(uint64_t *state);
 // [1]: Hybrid scalar/vector implementations of Keccak and SPHINCS+ on AArch64
 //      https://eprint.iacr.org/2022/1243.
 //
-// TODO: This needs replacing with a scalar/vector hybrid on non-Apple
-// systems pre Cortex-X4, as those don't have enough SIMD units implementing
-// SHA3 instructions to make them worthwhile on their own.
-#if defined(__ARM_FEATURE_SHA3)
+// For now, we only use this implementation on Apple M-series, as other
+// CPUs (pre Cortex-X4) tend to implement SHA3 instructions on a subset
+// of Neon units only, in which case a hybrid or the scalar implementation
+// should be used. As we don't yet have the hybrid here, we fall back to
+// scalar.
+#if defined(__ARM_FEATURE_SHA3) && defined(__APPLE__)
 
 #define MLKEM_USE_FIPS202_X1_ASM
 #define MLKEM_USE_FIPS202_X2_ASM
@@ -38,13 +40,18 @@ void keccak_f1600_x2_v84a_asm_clean(uint64_t *state);
 // Add this if the assembly implementation expects interleaved lanes
 // #define MLKEM_USE_FIPS202_X2_ASM_ZIPPED
 
-#elif 1
-// TODO: This should exclude CPUs with faster Barrel shift,
-//       notably excluding Cortex-A72.
+// We use lazy-rotation assembly from [1] for CPUs implementing v8.2A+
+// or later. The code itself is v8A-Neon, but lazy rotation is only beneficial
+// for fast Barrel shifters. Cortex-A72, which is v8.0-A, has slow Barrel
+// shifters, while Cortex-A76, which is v8.2-A, has fast Barrel shifters.
+// As an approximation, we thus pick the lazy rotation assembly v8.2A+.
+// If this happens to exclude CPUs with fast Barrel shifting (Cortex-A53 maybe?)
+// this logic needs refining.
+#elif defined(__ARM_ARCH) && __ARM_ARCH >= 802
 
 #define MLKEM_USE_FIPS202_X1_ASM
 
-#endif /* !__ARM_FEATURE_SHA3 */
+#endif /* !__ARM_FEATURE_SHA3 && __ARM_ARCH >= 802 */
 
 #endif /* !MLKEM_USE_FIPS202_ASM_FORCE */
 
