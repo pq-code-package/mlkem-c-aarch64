7f59bbc55fa570913e287ede5bbbb46ec016588e
Hanno Becker <beckphan@amazon.co.uk>
2024-09-20 14:05:09 +0100

    Add SLOTHY-optimized (A55) versions of mat-vec basemul
    Signed-off-by: Hanno Becker <beckphan@amazon.co.uk>

--- mlkem/asm/asm.h
+++ mlkem/asm/asm.h
@@ -23,25 +23,38 @@ void polyvec_basemul_acc_montgomery_cached_asm_k3_clean(
 void polyvec_basemul_acc_montgomery_cached_asm_k4_clean(
     int16_t *r, const int16_t *a, const int16_t *b, const int16_t *b_cache);
 
+void polyvec_basemul_acc_montgomery_cached_asm_k2_opt(
+    int16_t *r, const int16_t *a, const int16_t *b, const int16_t *b_cache);
+void polyvec_basemul_acc_montgomery_cached_asm_k3_opt(
+    int16_t *r, const int16_t *a, const int16_t *b, const int16_t *b_cache);
+void polyvec_basemul_acc_montgomery_cached_asm_k4_opt(
+    int16_t *r, const int16_t *a, const int16_t *b, const int16_t *b_cache);
+
+#define _polyvec_basemul_acc_montgomery_cached_asm_clean_name(k) \
+    polyvec_basemul_acc_montgomery_cached_asm_k ## k ## _clean
+#define polyvec_basemul_acc_montgomery_cached_asm_clean_name(k) \
+    _polyvec_basemul_acc_montgomery_cached_asm_clean_name(k)
+
+#define _polyvec_basemul_acc_montgomery_cached_asm_opt_name(k) \
+    polyvec_basemul_acc_montgomery_cached_asm_k ## k ## _opt
+#define polyvec_basemul_acc_montgomery_cached_asm_opt_name(k) \
+    _polyvec_basemul_acc_montgomery_cached_asm_opt_name(k)
+
 #if !defined(MLKEM_USE_NTT_ASM_FORCE)
 
 #if defined(MLKEM_USE_NTT_ASM_CLEAN)
 #define ntt_asm ntt_asm_clean
 #define intt_asm intt_asm_clean
 #define poly_reduce_asm poly_reduce_asm_clean
+#define polyvec_basemul_acc_montgomery_cached_asm \
+    polyvec_basemul_acc_montgomery_cached_asm_clean_name(KYBER_K)
 #else /* MLKEM_USE_NTT_ASM_CLEAN */
 #define ntt_asm ntt_asm_opt
 #define intt_asm intt_asm_opt
 #define poly_reduce_asm poly_reduce_asm_opt
-#endif /* !MLKEM_USE_NTT_ASM_CLEAN */
-
-#define _polyvec_basemul_acc_montgomery_cached_asm_name(k) \
-    polyvec_basemul_acc_montgomery_cached_asm_k ## k ## _clean
-#define polyvec_basemul_acc_montgomery_cached_asm_name(k) \
-    _polyvec_basemul_acc_montgomery_cached_asm_name(k)
 #define polyvec_basemul_acc_montgomery_cached_asm \
-    polyvec_basemul_acc_montgomery_cached_asm_name(KYBER_K)
-
+    polyvec_basemul_acc_montgomery_cached_asm_opt_name(KYBER_K)
+#endif /* !MLKEM_USE_NTT_ASM_CLEAN */
 
 #endif /* !MLKEM_USE_NTT_ASM_FORCE */
 
