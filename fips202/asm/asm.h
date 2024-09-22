09d6442a2370c7350687db951bb26d1ca360688a
Hanno Becker <beckphan@amazon.co.uk>
2024-09-22 06:12:48 +0100

    Use v8.4A-based x1 Keccak-f1600 on Apple CPUs
    Previous, we would use the lazy-rotation scalar assembly.
    Since Barrel shifting comes at a performance penalty on
    Apple CPUs (see https://dougallj.github.io/applecpu/firestorm.html),
    this implementation is slower than the standard C implementation.
    
    Moreover, the standard C implementation is slower than the
    x2-batched Neon implementation, restricted to one lane.
    
    This commit therefore changes the default Keccak-f1600 implementation
    on Apple CPUs to be a 1-fold Neon-based Keccak using SHA3 instructions.
    
    Signed-off-by: Hanno Becker <beckphan@amazon.co.uk>

--- fips202/asm/asm.h
+++ fips202/asm/asm.h
@@ -8,6 +8,7 @@
 
 #ifdef MLKEM_USE_AARCH64_ASM
 void keccak_f1600_x1_scalar_slothy_opt_a55(uint64_t *state);
+void keccak_f1600_x1_v84a_asm_clean(uint64_t *state);
 void keccak_f1600_x2_v84a_asm_clean(uint64_t *state);
 void keccak_f1600_x2_v8a_v84a_asm_hybrid(uint64_t *state);
 
@@ -21,18 +22,33 @@ void keccak_f1600_x2_v8a_v84a_asm_hybrid(uint64_t *state);
 
 #if !defined(MLKEM_USE_FIPS202_ASM_FORCE)
 
-// By default, we use lazy-rotation scalar assembly from [1].
-// NOTE: Lazy rotation is only beneficial for fast Barrel shifters.
-// For CPUs where barrel shifting is slow, like Cortex-A72, this
-// implementation should be disabled by setting SYS_AARCH64_SLOW_BARREL_SHIFTER.
-#if !defined(SYS_AARCH64_SLOW_BARREL_SHIFTER)
+// Keccak-f1600
+//
+// - On Arm-based Apple CPUs, we pick a pure Neon implementation.
+// - Otherwise, unless SYS_AARCH64_SLOW_BARREL_SHIFTER is set,
+//   we use lazy-rotation scalar assembly from [1].
+// - Otherwise, if SYS_AARCH64_SLOW_BARREL_SHIFTER is set, we
+//   fall back to the standard C implementation.
+#if defined(__ARM_FEATURE_SHA3) && defined(__APPLE__)
+#define MLKEM_USE_FIPS202_X1_ASM
+#define keccak_f1600_x1_asm keccak_f1600_x1_v84a_asm_clean
+#elif !defined(SYS_AARCH64_SLOW_BARREL_SHIFTER)
 #define MLKEM_USE_FIPS202_X1_ASM
 #define keccak_f1600_x1_asm keccak_f1600_x1_scalar_slothy_opt_a55
 #endif /* !SYS_AARCH64_SLOW_BARREL_SHIFTER */
 
-// If v8.4-A is implemented, leverage SHA3 instructions.
+// Keccak-f1600x2
 //
 // The optimal implementation is highly CPU-specific; see [1].
+//
+// For now, if v8.4-A is not implemented, we fall back to Keccak-f1600.
+// If v8.4-A is implemented and we are on an Apple CPU, we use a plain
+// Neon-based implementation.
+// If v8.4-A is implemented and we are not on an Apple CPU, we use a
+// Neon/Neon hybrid.
+// The reason for this distinction is that Apple CPUs appear to implement
+// the SHA3 instructions on all SIMD units, while Arm CPUs prior to Cortex-X4
+// don't, and ordinary Neon instructions are still needed.
 #if defined(__ARM_FEATURE_SHA3)
 // For Apple-M cores, we use a plain implementation leveraging SHA3
 // instructions only.
@@ -40,9 +56,6 @@ void keccak_f1600_x2_v8a_v84a_asm_hybrid(uint64_t *state);
 #define MLKEM_USE_FIPS202_X2_ASM
 #define keccak_f1600_x2_asm keccak_f1600_x2_v84a_asm_clean
 #else /* __APPLE__ */
-// Non Apple-M cores (pre Cortex-X4 at least) tend to implement SHA3
-// instructions on a subset of Neon units only, in which case a hybrid
-// Neon implementation is needed.
 #define MLKEM_USE_FIPS202_X2_ASM
 #define keccak_f1600_x2_asm keccak_f1600_x2_v8a_v84a_asm_hybrid
 #endif /* __APPLE__ */
