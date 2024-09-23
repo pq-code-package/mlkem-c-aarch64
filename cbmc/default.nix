9beb961b575651d7cd934cf9bb4096052a4251d4
Hanno Becker <beckphan@amazon.co.uk>
2024-09-23 05:15:56 +0100

    Update cbmc 6.3.1 (#150)
    * Update NIX config and Makefile.common for CBMC 6.3.1
    
    Signed-off-by: Rod Chapman <rodchap@amazon.com>
    
    * Update CBMC patch in Nix flake
    
    Signed-off-by: Hanno Becker <beckphan@amazon.co.uk>
    
    ---------
    
    Signed-off-by: Rod Chapman <rodchap@amazon.com>
    Signed-off-by: Hanno Becker <beckphan@amazon.co.uk>
    Co-authored-by: Rod Chapman <rodchap@amazon.com>

--- cbmc/default.nix
+++ cbmc/default.nix
@@ -6,12 +6,12 @@
 }:
 builtins.attrValues {
   cbmc = cbmc.overrideAttrs (old: rec {
-    version = "6.2.0"; # remember to adjust this in ../flake.nix too
+    version = "6.3.1"; # remember to adjust this in ../flake.nix too
     src = fetchFromGitHub {
       owner = "diffblue";
       repo = old.pname;
       rev = "${old.pname}-${version}";
-      hash = "sha256-WktGmkQpd7OXYEPgv0v7/+vwhTm1rERrjXIe6dzelFA=";
+      hash = "sha256-y3avPsVxtxSV+WB8TBbvnaNZ4WZltGRTcD+GPwTlp2E=";
     };
     patches = [
       ./0001-Do-not-download-sources-in-cmake.patch
