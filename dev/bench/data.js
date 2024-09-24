cb0bb8905e37f5eee19f5bc2d0db3e5260851aca
github-action-benchmark <github@users.noreply.github.com>
2024-09-24 08:53:14 +0000

    add Graviton2 (customSmallerIsBetter) benchmark result for 3f3fa3a842a2c3528036f6daaaf0091072e6843a

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1727167969608,
+  "lastUpdate": 1727167994302,
   "repoUrl": "https://github.com/pq-code-package/mlkem-c-aarch64",
   "entries": {
     "Arm Cortex-A72 (Raspberry Pi 4) benchmarks": [
@@ -16451,6 +16451,75 @@ window.BENCHMARK_DATA = {
           {
             "name": "ML-KEM-1024 decaps",
             "value": 178995,
+            "unit": "cycles"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "matthias@kannwischer.eu",
+            "name": "Matthias J. Kannwischer",
+            "username": "mkannwischer"
+          },
+          "committer": {
+            "email": "beckphan@amazon.co.uk",
+            "name": "Hanno Becker",
+            "username": "hanno-becker"
+          },
+          "distinct": true,
+          "id": "3f3fa3a842a2c3528036f6daaaf0091072e6843a",
+          "message": "Remove unused function poly_getnoise_eta1\n\nWe are using poly_getnoise_eta1_4x everywhere.\n\nSigned-off-by: Matthias J. Kannwischer <matthias@kannwischer.eu>",
+          "timestamp": "2024-09-24T09:49:07+01:00",
+          "tree_id": "c1f1d488faf0cbaaedaf6fed1368f1197094315c",
+          "url": "https://github.com/pq-code-package/mlkem-c-aarch64/commit/3f3fa3a842a2c3528036f6daaaf0091072e6843a"
+        },
+        "date": 1727167993283,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "ML-KEM-512 keypair",
+            "value": 60114,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-512 encaps",
+            "value": 63689,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-512 decaps",
+            "value": 76137,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-768 keypair",
+            "value": 103738,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-768 encaps",
+            "value": 104491,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-768 decaps",
+            "value": 120872,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-1024 keypair",
+            "value": 159931,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-1024 encaps",
+            "value": 158083,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-1024 decaps",
+            "value": 178341,
             "unit": "cycles"
           }
         ]
