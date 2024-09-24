fd265e54c12e9c2874297f11cddb386ac3fcb4a1
github-action-benchmark <github@users.noreply.github.com>
2024-09-24 16:43:01 +0800

    add Arm Cortex-A76 (Raspberry Pi 5) benchmarks (customSmallerIsBetter) benchmark result for 09d6442a2370c7350687db951bb26d1ca360688a

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1727142030971,
+  "lastUpdate": 1727167381048,
   "repoUrl": "https://github.com/pq-code-package/mlkem-c-aarch64",
   "entries": {
     "Arm Cortex-A72 (Raspberry Pi 4) benchmarks": [
@@ -10684,6 +10684,75 @@ window.BENCHMARK_DATA = {
           {
             "name": "ML-KEM-1024 decaps",
             "value": 180641,
+            "unit": "cycles"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "beckphan@amazon.co.uk",
+            "name": "Hanno Becker",
+            "username": "hanno-becker"
+          },
+          "committer": {
+            "email": "beckphan@amazon.co.uk",
+            "name": "Hanno Becker",
+            "username": "hanno-becker"
+          },
+          "distinct": true,
+          "id": "09d6442a2370c7350687db951bb26d1ca360688a",
+          "message": "Use v8.4A-based x1 Keccak-f1600 on Apple CPUs\n\nPrevious, we would use the lazy-rotation scalar assembly.\nSince Barrel shifting comes at a performance penalty on\nApple CPUs (see https://dougallj.github.io/applecpu/firestorm.html),\nthis implementation is slower than the standard C implementation.\n\nMoreover, the standard C implementation is slower than the\nx2-batched Neon implementation, restricted to one lane.\n\nThis commit therefore changes the default Keccak-f1600 implementation\non Apple CPUs to be a 1-fold Neon-based Keccak using SHA3 instructions.\n\nSigned-off-by: Hanno Becker <beckphan@amazon.co.uk>",
+          "timestamp": "2024-09-24T09:41:49+01:00",
+          "tree_id": "9ff414dcd8a1a725ba2e49b67a6e4904a3bbc520",
+          "url": "https://github.com/pq-code-package/mlkem-c-aarch64/commit/09d6442a2370c7350687db951bb26d1ca360688a"
+        },
+        "date": 1727167377120,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "ML-KEM-512 keypair",
+            "value": 59941,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-512 encaps",
+            "value": 62916,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-512 decaps",
+            "value": 75337,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-768 keypair",
+            "value": 103842,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-768 encaps",
+            "value": 105289,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-768 decaps",
+            "value": 121783,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-1024 keypair",
+            "value": 160721,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-1024 encaps",
+            "value": 160023,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-1024 decaps",
+            "value": 180660,
             "unit": "cycles"
           }
         ]
