1b194b4bef0a4bba30fc0ed6ae859490158817b6
Hanno Becker <beckphan@amazon.co.uk>
2024-09-26 11:37:05 +0100

    Give scalar [de]compress functions static linkage
    Signed-off-by: Hanno Becker <beckphan@amazon.co.uk>

--- mlkem/poly.h
+++ mlkem/poly.h
@@ -6,6 +6,7 @@
 #include <stdint.h>
 #include "params.h"
 #include "cbmc.h"
+#include "verify.h"
 
 /*
  * Elements of R_q = Z_q[X]/(X^n + 1). Represents polynomial
@@ -33,33 +34,33 @@ typedef struct
 #define scalar_decompress_q_32         KYBER_NAMESPACE(scalar_decompress_q_32)
 #define scalar_signed_to_unsigned_q_16 KYBER_NAMESPACE(scalar_signed_to_unsigned_q_16)
 
-uint32_t scalar_compress_q_16   (int32_t u)
+static inline uint32_t scalar_compress_q_16   (int32_t u)
 /* INDENT-OFF */
 __CPROVER_requires(0 <= u && u < KYBER_Q)
 __CPROVER_ensures(__CPROVER_return_value < 16)
 __CPROVER_ensures(__CPROVER_return_value == (((uint32_t) u * 16 + KYBER_Q / 2) / KYBER_Q) % 16);
 /* INDENT-ON */
 
-uint32_t scalar_decompress_q_16 (uint32_t u)
+static inline uint32_t scalar_decompress_q_16 (uint32_t u)
 /* INDENT-OFF */
 __CPROVER_requires(0 <= u && u < 16)
 __CPROVER_ensures(__CPROVER_return_value < KYBER_Q);
 /* INDENT-ON */
 
-uint32_t scalar_compress_q_32   (int32_t u)
+static inline uint32_t scalar_compress_q_32   (int32_t u)
 /* INDENT-OFF */
 __CPROVER_requires(0 <= u && u < KYBER_Q)
 __CPROVER_ensures(__CPROVER_return_value < 32)
 __CPROVER_ensures(__CPROVER_return_value == (((uint32_t) u * 32 + KYBER_Q / 2) / KYBER_Q) % 32);
 /* INDENT-ON */
 
-uint32_t scalar_decompress_q_32 (uint32_t u)
+static inline uint32_t scalar_decompress_q_32 (uint32_t u)
 /* INDENT-OFF */
 __CPROVER_requires(0 <= u && u < 32)
 __CPROVER_ensures(__CPROVER_return_value < KYBER_Q);
 /* INDENT-ON */
 
-uint16_t scalar_signed_to_unsigned_q_16 (int16_t c)
+static inline uint16_t scalar_signed_to_unsigned_q_16 (int16_t c)
 /* *INDENT-OFF* */
 __CPROVER_requires(c > -KYBER_Q) // c >= -3328
 __CPROVER_requires(c < KYBER_Q)  // c <= 3328
@@ -79,6 +80,119 @@ __CPROVER_requires(__CPROVER_forall { unsigned k; (k < KYBER_N) ==> ( -KYBER_Q <
 __CPROVER_assigns(__CPROVER_object_whole(r));
 /* *INDENT-ON* */
 
+/************************************************************
+ * Name: scalar_compress_q_16
+ *
+ * Description: Computes round(u * 16 / q)
+ *
+ * Arguments: - u: Unsigned canonical modulus modulo q
+ *                 to be compressed.
+ ************************************************************/
+static inline uint32_t scalar_compress_q_16(int32_t u)
+{
+    uint32_t d0 = (uint32_t) u;
+    d0 <<= 4;
+    d0 +=  1665;
+
+    /* This multiply will exceed UINT32_MAX and wrap around */
+    /* for large values of u. This is expected and required */
+    #ifdef CBMC
+#pragma CPROVER check push
+#pragma CPROVER check disable "unsigned-overflow"
+    #endif
+    d0 *=  80635;
+    #ifdef CBMC
+#pragma CPROVER check pop
+    #endif
+    d0 >>= 28;
+    d0 &=  0xF;
+    return d0;
+}
+
+/************************************************************
+ * Name: scalar_decompress_q_16
+ *
+ * Description: Computes round(u * q / 16)
+ *
+ * Arguments: - u: Unsigned canonical modulus modulo 16
+ *                 to be decompressed.
+ ************************************************************/
+static inline uint32_t scalar_decompress_q_16(uint32_t u)
+{
+    return ((u * KYBER_Q) + 8) / 16;
+}
+
+/************************************************************
+ * Name: scalar_compress_q_32
+ *
+ * Description: Computes round(u * 32 / q)
+ *
+ * Arguments: - u: Unsigned canonical modulus modulo q
+ *                 to be compressed.
+ ************************************************************/
+static inline uint32_t scalar_compress_q_32(int32_t u)
+{
+    uint32_t d0 = (uint32_t) u;
+    d0 <<= 5;
+    d0 +=  1664;
+
+    /* This multiply will exceed UINT32_MAX and wrap around */
+    /* for large values of u. This is expected and required */
+    #ifdef CBMC
+#pragma CPROVER check push
+#pragma CPROVER check disable "unsigned-overflow"
+    #endif
+    d0 *=  40318;
+    #ifdef CBMC
+#pragma CPROVER check pop
+    #endif
+    d0 >>= 27;
+    d0 &=  0x1f;
+    return d0;
+}
+
+/************************************************************
+ * Name: scalar_decompress_q_32
+ *
+ * Description: Computes round(u * q / 32)
+ *
+ * Arguments: - u: Unsigned canonical modulus modulo 32
+ *                 to be decompressed.
+ ************************************************************/
+static inline uint32_t scalar_decompress_q_32(uint32_t u)
+{
+    return ((u * KYBER_Q) + 16) / 32;
+}
+
+/************************************************************
+ * Name: scalar_signed_to_unsigned_q_16
+ *
+ * Description: converts signed polynomial coefficient
+ *              from signed (-3328 .. 3328) form to
+ *              unsigned form (0 .. 3328).
+ *
+ * Note: Cryptographic constant time implementation
+ *
+ * Examples:       0 -> 0
+ *                 1 -> 1
+ *              3328 -> 3328
+ *                -1 -> 3328
+ *                -2 -> 3327
+ *             -3328 -> 1
+ *
+ * Arguments: c: signed coefficient to be converted
+ ************************************************************/
+static inline uint16_t scalar_signed_to_unsigned_q_16 (int16_t c)
+{
+    // Add Q if c is negative, but in constant time
+    cmov_int16(&c, c + KYBER_Q, c < 0);
+
+    __CPROVER_assert(c >= 0, "scalar_signed_to_unsigned_q_16 result lower bound");
+    __CPROVER_assert(c < KYBER_Q, "scalar_signed_to_unsigned_q_16 result upper bound");
+
+    // and therefore cast to uint16_t is safe.
+    return (uint16_t) c;
+}
 
 #define poly_decompress KYBER_NAMESPACE(poly_decompress)
 void poly_decompress(poly *r, const uint8_t a[KYBER_POLYCOMPRESSEDBYTES])
