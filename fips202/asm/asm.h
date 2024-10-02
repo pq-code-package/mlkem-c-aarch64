a8502ee49de8ffa76f1a60eac388106315fd80c7
Hanno Becker <beckphan@amazon.co.uk>
2024-10-02 09:15:05 +0100

    Make FIPS202 ASM profile configurable
    Signed-off-by: Hanno Becker <beckphan@amazon.co.uk>

--- fips202/asm/asm.h
+++ fips202/asm/asm.h
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: Apache-2.0
-#ifndef ASM_H
-#define ASM_H
+#ifndef FIPS202_ASM_H
+#define FIPS202_ASM_H
 
 #include <stdint.h>
 #include "params.h"
@@ -14,63 +14,41 @@ void keccak_f1600_x2_v8a_v84a_asm_hybrid(uint64_t *state);
 void keccak_f1600_x4_scalar_v8a_asm_hybrid_opt(uint64_t *state);
 void keccak_f1600_x4_scalar_v84a_asm_hybrid_opt(uint64_t *state);
 void keccak_f1600_x4_scalar_v8a_v84a_asm_hybrid_opt(uint64_t *state);
-
-/*
- * Logic to decide which implementation to use.
- *
- * Source of implementations:
- * [1]: Hybrid scalar/vector implementations of Keccak and SPHINCS+ on AArch64
- *      https://eprint.iacr.org/2022/1243.
- */
-
-#if !defined(MLKEM_USE_FIPS202_ASM_FORCE)
-
-// Keccak-f1600
-//
-// - On Arm-based Apple CPUs, we pick a pure Neon implementation.
-// - Otherwise, unless SYS_AARCH64_SLOW_BARREL_SHIFTER is set,
-//   we use lazy-rotation scalar assembly from [1].
-// - Otherwise, if SYS_AARCH64_SLOW_BARREL_SHIFTER is set, we
-//   fall back to the standard C implementation.
-#if defined(__ARM_FEATURE_SHA3) && defined(__APPLE__)
-#define MLKEM_USE_FIPS202_X1_ASM
-#define keccak_f1600_x1_asm keccak_f1600_x1_v84a_asm_clean
-#elif !defined(SYS_AARCH64_SLOW_BARREL_SHIFTER)
-#define MLKEM_USE_FIPS202_X1_ASM
-#define keccak_f1600_x1_asm keccak_f1600_x1_scalar_slothy_opt_a55
-#endif /* !SYS_AARCH64_SLOW_BARREL_SHIFTER */
-
-// Keccak-f1600x2/x4
-//
-// The optimal implementation is highly CPU-specific; see [1].
-//
-// For now, if v8.4-A is not implemented, we fall back to Keccak-f1600.
-// If v8.4-A is implemented and we are on an Apple CPU, we use a plain
-// Neon-based implementation.
-// If v8.4-A is implemented and we are not on an Apple CPU, we use a
-// scalar/Neon/Neon hybrid.
-// The reason for this distinction is that Apple CPUs appear to implement
-// the SHA3 instructions on all SIMD units, while Arm CPUs prior to Cortex-X4
-// don't, and ordinary Neon instructions are still needed.
-#if defined(__ARM_FEATURE_SHA3)
-// For Apple-M cores, we use a plain implementation leveraging SHA3
-// instructions only.
-#if defined(__APPLE__)
-#define MLKEM_USE_FIPS202_X2_ASM
-#define keccak_f1600_x2_asm keccak_f1600_x2_v84a_asm_clean
-#else /* __APPLE__ */
-#define MLKEM_USE_FIPS202_X4_ASM
-#define keccak_f1600_x4_asm keccak_f1600_x4_scalar_v8a_v84a_asm_hybrid_opt
-#endif /* __APPLE__ */
-
-#else /* __ARM_FEATURE_SHA3 */
-
-#define MLKEM_USE_FIPS202_X4_ASM
-#define keccak_f1600_x4_asm keccak_f1600_x4_scalar_v8a_asm_hybrid_opt
-
-#endif /* __ARM_FEATURE_SHA3 */
-
-#endif /* !MLKEM_USE_FIPS202_ASM_FORCE */
 #endif /* MLKEM_USE_AARCH64_ASM */
 
-#endif
+/*
+ * The FIPS202 ASM profile decides which implementation(s) of FIPS202 to use.
+ *
+ * If you don't change anything, the default profile will be used. This profile
+ * picks implementations based on characteristics of your system visible to
+ * the compiler.
+ *
+ * The default logic is not perfect, and you may want to pick a specific
+ * profile for your target. There are three ways to do so, in descending
+ * order of convenience to the user:
+ * 1. Pick one of the profiles shipped with this repository.
+ * 2. Provide your own profile and register it via FIPS202_ASM_PROFILE
+ *    (which must be the profile's path relative to this directoru).
+ * 3. Set FIPS202_ASM_MANUAL and use an adhoc profile specified via CFLAGS.
+ */
+
+// Option 2: Manually written profile
+#if defined(FIPS202_ASM_PROFILE)
+
+#define STRINGIFY_(x) #x
+#define STRINGIFY(x) STRINGIFY_(x)
+#include STRINGIFY(FIPS202_ASM_PROFILE)
+
+// Option 1: Choose from shipped list of profiles
+#elif !defined(FIPS202_ASM_MANUAL)
+
+// Pick exactly one profile from the following list
+#include "profiles/default.h"
+
+#else /* !FIPS202_ASM_PROFILE && FIPS202_ASM_MANUAL */
+
+/* Option 3: Build your own profile here, or via CFLAGS */
+
+#endif /* !FIPS202_ASM_PROFILE && !FIPS202_ASM_MANUAL */
+
+#endif /* FIPS202_ASM_H */
