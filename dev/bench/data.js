bdb6c263440bd9d33e539bf2d7e7a9571ff4d494
github-action-benchmark <github@users.noreply.github.com>
2024-10-03 11:11:06 +0000

    add Graviton4 (customSmallerIsBetter) benchmark result for f46876af79a6f66669e3032ee3b6870b1d5b0286

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1727953786507,
+  "lastUpdate": 1727953866822,
   "repoUrl": "https://github.com/pq-code-package/mlkem-c-aarch64",
   "entries": {
     "Arm Cortex-A72 (Raspberry Pi 4) benchmarks": [
@@ -26169,6 +26169,77 @@ window.BENCHMARK_DATA = {
           {
             "name": "ML-KEM-1024 decaps",
             "value": 62516,
+            "unit": "cycles"
+          }
+        ]
+      }
+    ],
+    "Graviton4": [
+      {
+        "commit": {
+          "author": {
+            "email": "beckphan@amazon.co.uk",
+            "name": "Hanno Becker",
+            "username": "hanno-becker"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "f46876af79a6f66669e3032ee3b6870b1d5b0286",
+          "message": "Add Graviton4 benchmarks to CI (#189)\n\n* Add Graviton4 benchmarks to CI\r\n\r\nSigned-off-by: Hanno Becker <beckphan@amazon.co.uk>\r\n\r\n* Use armv9 as archflag for Gv4\r\n\r\nSigned-off-by: Hanno Becker <beckphan@amazon.co.uk>\r\n\r\n---------\r\n\r\nSigned-off-by: Hanno Becker <beckphan@amazon.co.uk>",
+          "timestamp": "2024-10-03T12:06:15+01:00",
+          "tree_id": "618a8fa0d91180b17c2487ddc75b302541d26b6a",
+          "url": "https://github.com/pq-code-package/mlkem-c-aarch64/commit/f46876af79a6f66669e3032ee3b6870b1d5b0286"
+        },
+        "date": 1727953865923,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "ML-KEM-512 keypair",
+            "value": 18882,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-512 encaps",
+            "value": 24886,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-512 decaps",
+            "value": 27996,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-768 keypair",
+            "value": 31802,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-768 encaps",
+            "value": 36712,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-768 decaps",
+            "value": 40883,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-1024 keypair",
+            "value": 46019,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-1024 encaps",
+            "value": 52916,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-1024 decaps",
+            "value": 58676,
             "unit": "cycles"
           }
         ]
