02ebb1a4e62d0b437611d48c48aa7ecf06e200be
github-action-benchmark <github@users.noreply.github.com>
2024-10-03 06:07:08 +0000

    add Arm Cortex-A72 (Raspberry Pi 4) benchmarks (customSmallerIsBetter) benchmark result for 8f21415a76ff394040e42a0c90d8a9fb06a253ce

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1727935575606,
+  "lastUpdate": 1727935627861,
   "repoUrl": "https://github.com/pq-code-package/mlkem-c-aarch64",
   "entries": {
     "Arm Cortex-A72 (Raspberry Pi 4) benchmarks": [
@@ -4574,6 +4574,75 @@ window.BENCHMARK_DATA = {
           {
             "name": "ML-KEM-1024 decaps",
             "value": 194357,
+            "unit": "cycles"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "beckphan@amazon.co.uk",
+            "name": "Hanno Becker",
+            "username": "hanno-becker"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "8f21415a76ff394040e42a0c90d8a9fb06a253ce",
+          "message": "Fix gen_matrix() (#185)\n\nWhen building the public matrix, we first squeeze GEN_MATRIX_NBLOCKS\r\nout of the Keccak state and rejection sample them into the matrix\r\nentries. We run a 'fill up' stage, squeezing and sampling one block a time,\r\nuntil the entry buffers are full.\r\n\r\nThe previous code got the fill-up stage wrong: Instead of squeezing one block\r\na time, we squeezed GEN_MATRIX_NBLOCKS, _but_ sampled as if we had only squeezed\r\none. This would lead to incorrect results if the fill-up stage needs more than\r\none iteration. Thanks to the large default value of GEN_MATRIX_NBLOCKS, this\r\nwould not happen in any of the KAT tests, but it may still fail in rare cases.\r\nIt also fails when setting GEN_MATRIX_NBLOCKS=2.\r\n\r\nSigned-off-by: Hanno Becker <beckphan@amazon.co.uk>",
+          "timestamp": "2024-10-03T07:01:50+01:00",
+          "tree_id": "2dd025390f3ace2d3fe3f4ff70e1c33c4ae523ca",
+          "url": "https://github.com/pq-code-package/mlkem-c-aarch64/commit/8f21415a76ff394040e42a0c90d8a9fb06a253ce"
+        },
+        "date": 1727935621629,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "ML-KEM-512 keypair",
+            "value": 58549,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-512 encaps",
+            "value": 69317,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-512 decaps",
+            "value": 77255,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-768 keypair",
+            "value": 105128,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-768 encaps",
+            "value": 117182,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-768 decaps",
+            "value": 127889,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-1024 keypair",
+            "value": 165475,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-1024 encaps",
+            "value": 179228,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-1024 decaps",
+            "value": 193992,
             "unit": "cycles"
           }
         ]
