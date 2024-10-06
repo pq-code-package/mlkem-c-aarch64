e27dddbb83cb8d8c79276ffbf49c186d7d381006
Hanno Becker <beckphan@amazon.co.uk>
2024-10-06 15:26:45 +0100

    Simplify C<->Native interface for mulcache computation (#204)
    Context: The mul[tiplication]cache of a polynomial is an opaque
    structure that implementations can use to cache precomputations
    needed during the repeated base multiplication of a polynomial
    in NTT form.
    
    Previously, the API for the computation of the mulcache would
    expose two arrays of constants which the AArch64 native implementation
    of the mulcache computation need.
    
    This commit removes those constants from the native API for the
    mulcache computation, since different native implementations are
    likely to require different sets of constants.
    
    Signed-off-by: Hanno Becker <beckphan@amazon.co.uk>

---
+++ mlkem/native/aarch64/consts.h
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: Apache-2.0
+
+#if !defined(MLKEM_NATIVE_AARCH64_CONSTS)
+#define MLKEM_NATIVE_AARCH64_CONSTS
+
+#include <stdint.h>
+
+extern const int16_t zetas_mulcache_native[256];
+extern const int16_t zetas_mulcache_twisted_native[256];
+
+#endif /* MLKEM_NATIVE_AARCH64_CONSTS */
