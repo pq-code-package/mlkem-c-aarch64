d0df54783124a654f033616ab542dca6bb3dd51d
github-action-benchmark <github@users.noreply.github.com>
2024-10-07 18:42:21 +0000

    add Arm Cortex-A55 (Snapdragon 888) benchmarks (customSmallerIsBetter) benchmark result for c4fefa0fb81eb2e92dd90afb23e740d726e675c6

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1728326446043,
+  "lastUpdate": 1728326541529,
   "repoUrl": "https://github.com/pq-code-package/mlkem-c-aarch64",
   "entries": {
     "Arm Cortex-A72 (Raspberry Pi 4) benchmarks": [
@@ -10941,6 +10941,73 @@ window.BENCHMARK_DATA = {
           {
             "name": "ML-KEM-1024 decaps",
             "value": 197299,
+            "unit": "cycles"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Hanno Becker",
+            "username": "hanno-becker",
+            "email": "beckphan@amazon.co.uk"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "c4fefa0fb81eb2e92dd90afb23e740d726e675c6",
+          "message": "Add AVX2 [inv]NTT from official Kyber repository (#203)\n\n* Add AVX2 NTT from official Kyber implementation\r\n\r\nThe AVX2 Kyber NTT produces non-reduced outputs in\r\nnon-standard order, for better performance. Neither\r\nis currently supported by the C<->Native interface.\r\n\r\nTo accommodate, we temporarily reduce and reorder the\r\noutput of the AVX2 NTT to match the current C<->Native\r\ninterface. This will likely need changing later.\r\n\r\nSigned-off-by: Hanno Becker <beckphan@amazon.co.uk>\r\n\r\n* Increase default alignment of buffers to 32 bytes\r\n\r\nSigned-off-by: Hanno Becker <beckphan@amazon.co.uk>\r\n\r\n* Add AVX2 invNTT from official Kyber implementation\r\n\r\nAs for the forward NTT, the inverse NTT operates on\r\na non-standard order that currently supported by our\r\nC<->Native interface. To accommodate, we shuffle the\r\ndata in the expected order first before calling the\r\nAVX2 iNTT. This may need revisiting later for better\r\nperformance.\r\n\r\nSigned-off-by: Hanno Becker <beckphan@amazon.co.uk>\r\n\r\n---------\r\n\r\nSigned-off-by: Hanno Becker <beckphan@amazon.co.uk>",
+          "timestamp": "2024-10-07T08:59:33Z",
+          "url": "https://github.com/pq-code-package/mlkem-c-aarch64/commit/c4fefa0fb81eb2e92dd90afb23e740d726e675c6"
+        },
+        "date": 1728326533777,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "ML-KEM-512 keypair",
+            "value": 60154,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-512 encaps",
+            "value": 75381,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-512 decaps",
+            "value": 86084,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-768 keypair",
+            "value": 102071,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-768 encaps",
+            "value": 120744,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-768 decaps",
+            "value": 135488,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-1024 keypair",
+            "value": 153869,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-1024 encaps",
+            "value": 177226,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-1024 decaps",
+            "value": 197614,
             "unit": "cycles"
           }
         ]
