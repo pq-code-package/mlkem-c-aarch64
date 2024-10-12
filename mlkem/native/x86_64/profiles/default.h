353da0465dbf61b16d468766d7a7ad8cf93ff536
Hanno Becker <beckphan@amazon.co.uk>
2024-10-12 06:31:09 +0100

    Add AVX2 base multiplication
    Signed-off-by: Hanno Becker <beckphan@amazon.co.uk>

--- mlkem/native/x86_64/profiles/default.h
+++ mlkem/native/x86_64/profiles/default.h
@@ -7,6 +7,8 @@
 #else
 #define MLKEM_ARITH_NATIVE_PROFILE_H
 
+#include <string.h>
+
 #include "../../arith_native.h"
 #include "../arith_native_x86_64.h"
 #include "../consts.h"
@@ -17,6 +19,8 @@
 #define MLKEM_USE_NATIVE_NTT
 #define MLKEM_USE_NATIVE_INTT
 #define MLKEM_USE_NATIVE_POLY_REDUCE
+#define MLKEM_USE_NATIVE_POLYVEC_BASEMUL_ACC_MONTGOMERY_CACHED
+#define MLKEM_USE_NATIVE_POLY_MULCACHE_COMPUTE
 
 #define INVNTT_BOUND_NATIVE (KYBER_Q + 1)  // poly_reduce() is in [0,..,KYBER_Q]
 #define NTT_BOUND_NATIVE (KYBER_Q + 1)     // poly_reduce() is in [0,..,KYBER_Q]
@@ -52,6 +56,24 @@ static inline void intt_native(poly *data) {
 
 static inline void poly_reduce_native(poly *data) {
   reduce_avx2((__m256i *)data->coeffs, qdata.vec);
+}
+
+static inline void poly_mulcache_compute_native(poly_mulcache *x,
+                                                const poly *y) {
+  // AVX2 backend does not use mulcache
+  ((void)x);
+  ((void)y);
+
+  // TODO! The mulcache is subject to the absolute bound < q
+  // This needs to be dropped if the mulcache is not present.
+  // Until that's done, memset to 0 to avoid failure.
+  memset(x, 0, sizeof(poly_mulcache));
+}
+
+static inline void polyvec_basemul_acc_montgomery_cached_native(
+    poly *r, const polyvec *a, const polyvec *b,
+    const polyvec_mulcache *b_cache) {
+  polyvec_basemul_acc_montgomery_cached_avx2(r, a, b, b_cache);
 }
 
 #endif /* MLKEM_ARITH_NATIVE_PROFILE_H */
