413f166e49949a52ec4d1ae2feae4de03abb33ac
github-action-benchmark <github@users.noreply.github.com>
2024-10-13 08:39:58 +0000

    add Graviton4 (customSmallerIsBetter) benchmark result for bbfaa0c6aa8b7d777d6929d289bb7c5c1cdf38d3

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1728808781849,
+  "lastUpdate": 1728808798480,
   "repoUrl": "https://github.com/pq-code-package/mlkem-c-aarch64",
   "entries": {
     "Arm Cortex-A72 (Raspberry Pi 4) benchmarks": [
@@ -32990,6 +32990,75 @@ window.BENCHMARK_DATA = {
           {
             "name": "ML-KEM-1024 decaps",
             "value": 57547,
+            "unit": "cycles"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "beckphan@amazon.co.uk",
+            "name": "Hanno Becker",
+            "username": "hanno-becker"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "bbfaa0c6aa8b7d777d6929d289bb7c5c1cdf38d3",
+          "message": "AVX2: Switch to custom NTT coefficient ordering  (#219)\n\n* AVX2: Switch to custom NTT domain coefficient ordering\r\n\r\nSigned-off-by: Hanno Becker <beckphan@amazon.co.uk>\r\n\r\n* Remove unnecessary conditional subtraction in AVX2 poly_tobytes()\r\n\r\nA prior commit has instead adjusted `poly_reduce()` to already\r\nensure unsigned canonical representatives, so the conditional\r\nsubtraction in the AVX2 assembly for poly_tobytes() has no effect.\r\n\r\nSigned-off-by: Hanno Becker <beckphan@amazon.co.uk>\r\n\r\n---------\r\n\r\nSigned-off-by: Hanno Becker <beckphan@amazon.co.uk>",
+          "timestamp": "2024-10-13T16:36:48+08:00",
+          "tree_id": "2af53f747ecfd4525e716c580b207bd87bab4b2c",
+          "url": "https://github.com/pq-code-package/mlkem-c-aarch64/commit/bbfaa0c6aa8b7d777d6929d289bb7c5c1cdf38d3"
+        },
+        "date": 1728808796987,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "ML-KEM-512 keypair",
+            "value": 18721,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-512 encaps",
+            "value": 23566,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-512 decaps",
+            "value": 26696,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-768 keypair",
+            "value": 31586,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-768 encaps",
+            "value": 35389,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-768 decaps",
+            "value": 39703,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-1024 keypair",
+            "value": 45633,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-1024 encaps",
+            "value": 51614,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-1024 decaps",
+            "value": 57529,
             "unit": "cycles"
           }
         ]
