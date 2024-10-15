e8ebabea03398e9de9b1b4d70d074e59f2d8b381
github-action-benchmark <github@users.noreply.github.com>
2024-10-15 04:49:37 +0000

    add Graviton3 (customSmallerIsBetter) benchmark result for 30855f1a4a9d7c54985e80b337a9fd03235a9fbc

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1728967721959,
+  "lastUpdate": 1728967777679,
   "repoUrl": "https://github.com/pq-code-package/mlkem-c-aarch64",
   "entries": {
     "Arm Cortex-A72 (Raspberry Pi 4) benchmarks": [
@@ -34163,6 +34163,75 @@ window.BENCHMARK_DATA = {
           {
             "name": "ML-KEM-1024 decaps",
             "value": 61207,
+            "unit": "cycles"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "beckphan@amazon.co.uk",
+            "name": "Hanno Becker",
+            "username": "hanno-becker"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "30855f1a4a9d7c54985e80b337a9fd03235a9fbc",
+          "message": "Set C standard to C99 (#225)\n\n* Set C standard to C99\r\n\r\nMLKEM-C-AArch64 strives to be compatible with a wide range of\r\n(old) compilers. Force restriction to C99 to facilitate this.\r\n\r\nSigned-off-by: Hanno Becker <beckphan@amazon.co.uk>\r\n\r\n* Ensure syscall() is declared even if -std=c99\r\n\r\nSigned-off-by: Hanno Becker <beckphan@amazon.co.uk>\r\n\r\n---------\r\n\r\nSigned-off-by: Hanno Becker <beckphan@amazon.co.uk>",
+          "timestamp": "2024-10-15T12:45:05+08:00",
+          "tree_id": "cfe8a46b43a71e5a1fc076b5b56fff12bb32dc55",
+          "url": "https://github.com/pq-code-package/mlkem-c-aarch64/commit/30855f1a4a9d7c54985e80b337a9fd03235a9fbc"
+        },
+        "date": 1728967775698,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "ML-KEM-512 keypair",
+            "value": 19438,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-512 encaps",
+            "value": 24785,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-512 decaps",
+            "value": 28421,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-768 keypair",
+            "value": 33151,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-768 encaps",
+            "value": 37493,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-768 decaps",
+            "value": 42157,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-1024 keypair",
+            "value": 47924,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-1024 encaps",
+            "value": 55074,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-1024 decaps",
+            "value": 61724,
             "unit": "cycles"
           }
         ]
