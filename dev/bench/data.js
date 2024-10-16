05ee404f59406c7a1b8e93278c4de8e36ff37b24
github-action-benchmark <github@users.noreply.github.com>
2024-10-16 15:58:49 +0000

    add Arm Cortex-A72 (Raspberry Pi 4) benchmarks (customSmallerIsBetter) benchmark result for 09b50fe1ef16827ce0a440913550cd5c432e7573

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1729094308410,
+  "lastUpdate": 1729094329358,
   "repoUrl": "https://github.com/pq-code-package/mlkem-c-aarch64",
   "entries": {
     "Arm Cortex-A72 (Raspberry Pi 4) benchmarks": [
@@ -6916,6 +6916,75 @@ window.BENCHMARK_DATA = {
           {
             "name": "ML-KEM-1024 decaps",
             "value": 163463,
+            "unit": "cycles"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "rodchap@amazon.com",
+            "name": "Roderick Chapman",
+            "username": "rod-chapman"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "09b50fe1ef16827ce0a440913550cd5c432e7573",
+          "message": "CBMC proofs for type-safety of reduce.c (#232)\n\n* Add CBMC proof for `montgomery_reduce()`\r\n\r\nSigned-off-by: Rod Chapman <rodchap@amazon.com>\r\n\r\n* Add proof of fqmul()\r\n\r\nIn particular, this proof shows that for any parameters a and b,\r\nthe pre-condition of montgomery_reduce() is satisfied.\r\n\r\nSigned-off-by: Rod Chapman <rodchap@amazon.com>\r\n\r\n* Add CBMC proof for `barrett_reduce()`\r\n\r\nSigned-off-by: Rod Chapman <rodchap@amazon.com>\r\n\r\n---------\r\n\r\nSigned-off-by: Rod Chapman <rodchap@amazon.com>",
+          "timestamp": "2024-10-16T16:54:54+01:00",
+          "tree_id": "cd501ac21c61893dd41199d585518b5030f513b2",
+          "url": "https://github.com/pq-code-package/mlkem-c-aarch64/commit/09b50fe1ef16827ce0a440913550cd5c432e7573"
+        },
+        "date": 1729094318273,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "ML-KEM-512 keypair",
+            "value": 53215,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-512 encaps",
+            "value": 61212,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-512 decaps",
+            "value": 69656,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-768 keypair",
+            "value": 89277,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-768 encaps",
+            "value": 99761,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-768 decaps",
+            "value": 111305,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-1024 keypair",
+            "value": 135040,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-1024 encaps",
+            "value": 148465,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-1024 decaps",
+            "value": 163118,
             "unit": "cycles"
           }
         ]
