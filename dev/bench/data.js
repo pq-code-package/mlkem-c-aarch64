3d3656ff02778344da437d5d3cd74a94b18e6332
github-action-benchmark <github@users.noreply.github.com>
2024-10-16 19:24:11 +0000

    add Graviton2 (customSmallerIsBetter) benchmark result for 59a0b06557e8f229a7bb92315c31c7857211797d

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1729106639753,
+  "lastUpdate": 1729106651067,
   "repoUrl": "https://github.com/pq-code-package/mlkem-c-aarch64",
   "entries": {
     "Arm Cortex-A72 (Raspberry Pi 4) benchmarks": [
@@ -29062,6 +29062,75 @@ window.BENCHMARK_DATA = {
           {
             "name": "ML-KEM-1024 decaps",
             "value": 94695,
+            "unit": "cycles"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "rodchap@amazon.com",
+            "name": "Roderick Chapman",
+            "username": "rod-chapman"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "59a0b06557e8f229a7bb92315c31c7857211797d",
+          "message": "Add proof artefacts for poly_tobytes() (#235)\n\n* Add proof artefacts for poly_tobytes()\r\n\r\nCode updated to add contracts, and make type\r\nconversions more explicit for CBMC.\r\n\r\nSigned-off-by: Rod Chapman <rodchap@amazon.com>\r\n\r\n* Address review comments on poly.c/poly_tobytes() proof\r\n\r\n1. Remove needless masks where CBMC can deduce the range.\r\n\r\n2. Shorten loop invariant range constraint on i onto a single line.\r\n\r\nSigned-off-by: Rod Chapman <rodchap@amazon.com>\r\n\r\n* Remove needless casts to (uint8_t)\r\n\r\nSigned-off-by: Rod Chapman <rodchap@amazon.com>\r\n\r\n---------\r\n\r\nSigned-off-by: Rod Chapman <rodchap@amazon.com>",
+          "timestamp": "2024-10-16T20:20:18+01:00",
+          "tree_id": "9f872fe3fdc4b1bcfb00dbcc4e419542819db440",
+          "url": "https://github.com/pq-code-package/mlkem-c-aarch64/commit/59a0b06557e8f229a7bb92315c31c7857211797d"
+        },
+        "date": 1729106649239,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "ML-KEM-512 keypair",
+            "value": 29754,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-512 encaps",
+            "value": 37199,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-512 decaps",
+            "value": 43301,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-768 keypair",
+            "value": 50245,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-768 encaps",
+            "value": 57678,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-768 decaps",
+            "value": 65612,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-1024 keypair",
+            "value": 73799,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-1024 encaps",
+            "value": 83905,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-1024 decaps",
+            "value": 94693,
             "unit": "cycles"
           }
         ]
