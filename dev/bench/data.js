288900cd59ebec286ca5a48798b39255d8bcf52a
github-action-benchmark <github@users.noreply.github.com>
2024-10-17 08:19:51 +0000

    add AMD EPYC 4th gen (c7a) (customSmallerIsBetter) benchmark result for bc4d1fd27d55c2ef95082a4181cc4f939f9af967

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1729153189145,
+  "lastUpdate": 1729153191696,
   "repoUrl": "https://github.com/pq-code-package/mlkem-c-aarch64",
   "entries": {
     "Arm Cortex-A72 (Raspberry Pi 4) benchmarks": [
@@ -48239,6 +48239,75 @@ window.BENCHMARK_DATA = {
           {
             "name": "ML-KEM-1024 decaps",
             "value": 77707,
+            "unit": "cycles"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "beckphan@amazon.co.uk",
+            "name": "Hanno Becker",
+            "username": "hanno-becker"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "bc4d1fd27d55c2ef95082a4181cc4f939f9af967",
+          "message": "Merge pull request #250 from pq-code-package/ci_tests\n\nAdd `tests func` to quickcheck CI",
+          "timestamp": "2024-10-17T09:16:51+01:00",
+          "tree_id": "3c979b4c9cb8bfad0c8e21e304bf7a7e17bac280",
+          "url": "https://github.com/pq-code-package/mlkem-c-aarch64/commit/bc4d1fd27d55c2ef95082a4181cc4f939f9af967"
+        },
+        "date": 1729153189603,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "ML-KEM-512 keypair",
+            "value": 24637,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-512 encaps",
+            "value": 32754,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-512 decaps",
+            "value": 35646,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-768 keypair",
+            "value": 43441,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-768 encaps",
+            "value": 49072,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-768 decaps",
+            "value": 53068,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-1024 keypair",
+            "value": 63385,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-1024 encaps",
+            "value": 72033,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-1024 decaps",
+            "value": 77739,
             "unit": "cycles"
           }
         ]
