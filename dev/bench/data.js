a25ec321202e50ff16af93444314ffe2886f07a3
github-action-benchmark <github@users.noreply.github.com>
2024-10-17 15:11:36 +0000

    add AMD EPYC 4th gen (c7a) (customSmallerIsBetter) benchmark result for d00022955386c2d22631dedbcd29f88d80db91e1

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1729177886831,
+  "lastUpdate": 1729177896860,
   "repoUrl": "https://github.com/pq-code-package/mlkem-c-aarch64",
   "entries": {
     "Arm Cortex-A72 (Raspberry Pi 4) benchmarks": [
@@ -48998,6 +48998,75 @@ window.BENCHMARK_DATA = {
           {
             "name": "ML-KEM-1024 decaps",
             "value": 77739,
+            "unit": "cycles"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "beckphan@amazon.co.uk",
+            "name": "Hanno Becker",
+            "username": "hanno-becker"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "d00022955386c2d22631dedbcd29f88d80db91e1",
+          "message": "Merge pull request #233 from pq-code-package/pin-clang-format-version\n\nNix cache enhancement in CI and ping clang-tools version to `17.0.6`",
+          "timestamp": "2024-10-17T16:07:27+01:00",
+          "tree_id": "d2438e435a44c4f026e4c90ce4ef802a81693c66",
+          "url": "https://github.com/pq-code-package/mlkem-c-aarch64/commit/d00022955386c2d22631dedbcd29f88d80db91e1"
+        },
+        "date": 1729177894816,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "ML-KEM-512 keypair",
+            "value": 24659,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-512 encaps",
+            "value": 32735,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-512 decaps",
+            "value": 35633,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-768 keypair",
+            "value": 43431,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-768 encaps",
+            "value": 49048,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-768 decaps",
+            "value": 53061,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-1024 keypair",
+            "value": 63392,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-1024 encaps",
+            "value": 72040,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-1024 decaps",
+            "value": 77691,
             "unit": "cycles"
           }
         ]
