0e1e313a5e8a9b17b3dd1a9ee19104de7c826ab2
github-action-benchmark <github@users.noreply.github.com>
2024-10-30 09:36:35 +0000

    add Graviton2 (customSmallerIsBetter) benchmark result for 9b06cdccce5379f41dcb9260fb25f200603ec268

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1730280929429,
+  "lastUpdate": 1730280995803,
   "repoUrl": "https://github.com/pq-code-package/mlkem-c-aarch64",
   "entries": {
     "Arm Cortex-A72 (Raspberry Pi 4) benchmarks": [
@@ -33892,6 +33892,75 @@ window.BENCHMARK_DATA = {
           {
             "name": "ML-KEM-1024 decaps",
             "value": 94892,
+            "unit": "cycles"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "beckphan@amazon.co.uk",
+            "name": "Hanno Becker",
+            "username": "hanno-becker"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "9b06cdccce5379f41dcb9260fb25f200603ec268",
+          "message": "Merge pull request #277 from pq-code-package/fix_276\n\nFix quoting of CFLAGS when fwding from `tests all` to `tests func/kat/nistkat`",
+          "timestamp": "2024-10-30T09:28:33Z",
+          "tree_id": "95398f7147f25d169f04018f2543b64490cf4733",
+          "url": "https://github.com/pq-code-package/mlkem-c-aarch64/commit/9b06cdccce5379f41dcb9260fb25f200603ec268"
+        },
+        "date": 1730280993252,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "ML-KEM-512 keypair",
+            "value": 29783,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-512 encaps",
+            "value": 37225,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-512 decaps",
+            "value": 43295,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-768 keypair",
+            "value": 50295,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-768 encaps",
+            "value": 57552,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-768 decaps",
+            "value": 65783,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-1024 keypair",
+            "value": 73751,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-1024 encaps",
+            "value": 83949,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-1024 decaps",
+            "value": 94883,
             "unit": "cycles"
           }
         ]
