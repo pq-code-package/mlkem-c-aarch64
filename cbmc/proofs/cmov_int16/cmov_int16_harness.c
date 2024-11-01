4ce233ade17d6a38a425b645dc9a894f6ebcc538
Rod Chapman <rodchap@amazon.com>
2024-11-01 12:44:09 +0000

    Adds contract-based proof of cmov_int16()
    Signed-off-by: Rod Chapman <rodchap@amazon.com>

---
+++ cbmc/proofs/cmov_int16/cmov_int16_harness.c
@@ -0,0 +1,29 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: MIT-0 AND Apache-2.0
+
+/*
+ * Insert copyright notice
+ */
+
+/**
+ * @file cmov_int16_harness.c
+ * @brief Implements the proof harness for cmov_int16 function.
+ */
+#include "verify.h"
+
+/*
+ * Insert project header files that
+ *   - include the declaration of the function
+ *   - include the types needed to declare function arguments
+ */
+
+/**
+ * @brief Starting point for formal analysis
+ *
+ */
+void harness(void) {
+  uint16_t b;
+  int16_t r, v;
+
+  cmov_int16(&r, v, b);
+}
--- mlkem/verify.h
+++ mlkem/verify.h
@@ -4,6 +4,7 @@
 
 #include <stddef.h>
 #include <stdint.h>
+#include "cbmc.h"
 #include "params.h"
 
 #define verify MLKEM_NAMESPACE(verify)
@@ -13,6 +14,22 @@ int verify(const uint8_t *a, const uint8_t *b, size_t len);
 void cmov(uint8_t *r, const uint8_t *x, size_t len, uint8_t b);
 
 #define cmov_int16 MLKEM_NAMESPACE(cmov_int16)
-void cmov_int16(int16_t *r, int16_t v, uint16_t b);
-
+/*************************************************
+ * Name:        cmov_int16
+ *
+ * Description: Copy input v to *r if b is 1, don't modify *r if b is 0.
+ *              Requires b to be in {0,1};
+ *              Runs in constant time.
+ *
+ * Arguments:   int16_t *r:       pointer to output int16_t
+ *              int16_t v:        input int16_t. Must not be NULL
+ *              uint16_t b:       Condition bit; has to be in {0,1}
+ **************************************************/
+void cmov_int16(int16_t *r, const int16_t v, const uint16_t b)
+    // clang-format off
+REQUIRES(b == 0 || b == 1)
+REQUIRES(r != NULL && IS_FRESH(r, sizeof(int16_t)))
+ASSIGNS(OBJECT_WHOLE(r))
+ENSURES(*r == (b ? v : OLD(*r)));
+// clang-format on
 #endif
