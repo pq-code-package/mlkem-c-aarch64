90d0e7b09892e933a074d8c7e923c08310f62c66
Hanno Becker <beckphan@amazon.co.uk>
2024-11-02 21:27:37 +0000

    CBMC: Add tentative specs for shake256[x4] and mlkem_shake256_prf
    For now, we axiomatize the behaviour of those functions through
    their specs, enabling reasoning of higher-level code. Ultimately,
    of course, they need (refining and) proving.
    
    Signed-off-by: Hanno Becker <beckphan@amazon.co.uk>

--- fips202/fips202x4.h
+++ fips202/fips202x4.h
@@ -7,6 +7,8 @@
 #include "keccakf1600.h"
 #include "namespace.h"
 
+#include "cbmc.h"
+
 #define shake128x4_absorb FIPS202_NAMESPACE(shake128x4_absorb)
 void shake128x4_absorb(keccakx4_state *state, const uint8_t *in0,
                        const uint8_t *in1, const uint8_t *in2,
@@ -30,6 +32,20 @@ void shake256x4_squeezeblocks(uint8_t *out0, uint8_t *out1, uint8_t *out2,
 #define shake256x4 FIPS202_NAMESPACE(shake256x4)
 void shake256x4(uint8_t *out0, uint8_t *out1, uint8_t *out2, uint8_t *out3,
                 size_t outlen, uint8_t *in0, uint8_t *in1, uint8_t *in2,
-                uint8_t *in3, size_t inlen);
+                uint8_t *in3, size_t inlen)  // clang-format off
+// Refine +prove this spec, e.g. add disjointness constraints?
+REQUIRES(READABLE(in0, inlen))
+REQUIRES(READABLE(in1, inlen))
+REQUIRES(READABLE(in2, inlen))
+REQUIRES(READABLE(in3, inlen))
+REQUIRES(WRITEABLE(out0, outlen))
+REQUIRES(WRITEABLE(out1, outlen))
+REQUIRES(WRITEABLE(out2, outlen))
+REQUIRES(WRITEABLE(out3, outlen))
+ASSIGNS(OBJECT_UPTO(out0, outlen))
+ASSIGNS(OBJECT_UPTO(out1, outlen))
+ASSIGNS(OBJECT_UPTO(out2, outlen))
+ASSIGNS(OBJECT_UPTO(out3, outlen));
+// clang-format on
 
 #endif
--- mlkem/symmetric.h
+++ mlkem/symmetric.h
@@ -8,14 +8,21 @@
 
 #include "fips202.h"
 
+#include "cbmc.h"
+
 #define mlkem_shake256_prf MLKEM_NAMESPACE(mlkem_shake256_prf)
 void mlkem_shake256_prf(uint8_t *out, size_t outlen,
                         const uint8_t key[MLKEM_SYMBYTES], uint8_t nonce);
 
 #define mlkem_shake256_rkprf MLKEM_NAMESPACE(mlkem_shake256_rkprf)
-void mlkem_shake256_rkprf(uint8_t out[MLKEM_SSBYTES],
-                          const uint8_t key[MLKEM_SYMBYTES],
-                          const uint8_t input[MLKEM_CIPHERTEXTBYTES]);
+void mlkem_shake256_rkprf(
+    uint8_t out[MLKEM_SSBYTES], const uint8_t key[MLKEM_SYMBYTES],
+    const uint8_t input[MLKEM_CIPHERTEXTBYTES])  // clang-format off
+// TODO: Refine + prove this spec, e.g. add disjointness constraints?
+REQUIRES(READABLE(input, MLKEM_CIPHERTEXTBYTES))
+REQUIRES(READABLE(key, MLKEM_SYMBYTES))
+REQUIRES(WRITEABLE(out, MLKEM_SSBYTES))
+ASSIGNS(OBJECT_UPTO(out, MLKEM_SSBYTES));  // clang-format on
 
 #define hash_h(OUT, IN, INBYTES) sha3_256(OUT, IN, INBYTES)
 #define hash_g(OUT, IN, INBYTES) sha3_512(OUT, IN, INBYTES)
