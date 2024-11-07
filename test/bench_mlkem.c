ba54e364a66539a6d25f57767de5b6e08cc308b2
Matthias J. Kannwischer <matthias@kannwischer.eu>
2024-11-07 17:07:39 +0800

    add bench
    Signed-off-by: Matthias J. Kannwischer <matthias@kannwischer.eu>

--- test/bench_mlkem.c
+++ test/bench_mlkem.c
@@ -17,33 +17,37 @@ static int cmp_uint64_t(const void *a, const void *b) {
 }
 
 static void print_median(const char *txt, uint64_t cyc[NTESTS]) {
-  printf("%10s cycles = %" PRIu64 "\n", txt, cyc[NTESTS >> 1] / NITERATIONS);
+  printf("%14s cycles = %" PRIu64 "\n", txt, cyc[NTESTS >> 1] / NITERATIONS);
 }
 
 static int percentiles[] = {1, 10, 20, 30, 40, 50, 60, 70, 80, 90, 99};
 
 static void print_percentile_legend(void) {
-  printf("%21s", "percentile");
+  printf("%27s", "percentile");
   for (unsigned i = 0; i < sizeof(percentiles) / sizeof(percentiles[0]); i++)
     printf("%7d", percentiles[i]);
   printf("\n");
 }
 
 static void print_percentiles(const char *txt, uint64_t cyc[NTESTS]) {
-  printf("%10s percentiles:", txt);
+  printf("%14s percentiles:", txt);
   for (unsigned i = 0; i < sizeof(percentiles) / sizeof(percentiles[0]); i++)
     printf("%7" PRIu64, (cyc)[NTESTS * percentiles[i] / 100] / NITERATIONS);
   printf("\n");
 }
 
 static int bench(void) {
-  uint8_t pk[CRYPTO_PUBLICKEYBYTES];
-  uint8_t sk[CRYPTO_SECRETKEYBYTES];
+  mlkem_public_key pk;
+  mlkem_secret_key sk;
+  uint8_t pks[CRYPTO_PUBLICKEYBYTES];
+  uint8_t sks[CRYPTO_SECRETKEYBYTES];
   uint8_t ct[CRYPTO_CIPHERTEXTBYTES];
   uint8_t key_a[CRYPTO_BYTES];
   uint8_t key_b[CRYPTO_BYTES];
   unsigned char kg_rand[2 * CRYPTO_BYTES], enc_rand[CRYPTO_BYTES];
   uint64_t cycles_kg[NTESTS], cycles_enc[NTESTS], cycles_dec[NTESTS];
+  uint64_t cycles_ser_sk[NTESTS], cycles_deser_sk[NTESTS],
+      cycles_ser_pk[NTESTS], cycles_deser_pk[NTESTS];
 
   unsigned int i, j;
   uint64_t t0, t1;
@@ -55,35 +59,87 @@ static int bench(void) {
 
     // Key-pair generation
     for (j = 0; j < NWARMUP; j++) {
-      crypto_kem_keypair_derand(pk, sk, kg_rand);
+      crypto_kem_keypair_derand(&pk, &sk, kg_rand);
     }
 
     t0 = get_cyclecounter();
     for (j = 0; j < NITERATIONS; j++) {
-      crypto_kem_keypair_derand(pk, sk, kg_rand);
+      crypto_kem_keypair_derand(&pk, &sk, kg_rand);
     }
     t1 = get_cyclecounter();
     cycles_kg[i] = t1 - t0;
 
 
+    // Serialize secret key
+    for (j = 0; j < NWARMUP; j++) {
+      crypto_kem_serialize_sk(sks, &sk);
+    }
+
+    t0 = get_cyclecounter();
+    for (j = 0; j < NITERATIONS; j++) {
+      crypto_kem_serialize_sk(sks, &sk);
+    }
+    t1 = get_cyclecounter();
+    cycles_ser_sk[i] = t1 - t0;
+
+
+
+    // Serialize public key
+    for (j = 0; j < NWARMUP; j++) {
+      crypto_kem_serialize_pk(pks, &pk);
+    }
+
+    t0 = get_cyclecounter();
+    for (j = 0; j < NITERATIONS; j++) {
+      crypto_kem_serialize_pk(pks, &pk);
+    }
+    t1 = get_cyclecounter();
+    cycles_ser_pk[i] = t1 - t0;
+
+
+    // Deserialize public key
+    for (j = 0; j < NWARMUP; j++) {
+      crypto_kem_deserialize_pk(&pk, pks);
+    }
+
+    t0 = get_cyclecounter();
+    for (j = 0; j < NITERATIONS; j++) {
+      crypto_kem_deserialize_pk(&pk, pks);
+    }
+    t1 = get_cyclecounter();
+    cycles_deser_pk[i] = t1 - t0;
+
+
     // Encapsulation
     for (j = 0; j < NWARMUP; j++) {
-      crypto_kem_enc_derand(ct, key_a, pk, enc_rand);
+      crypto_kem_enc_derand(ct, key_a, &pk, enc_rand);
     }
     t0 = get_cyclecounter();
     for (j = 0; j < NITERATIONS; j++) {
-      crypto_kem_enc_derand(ct, key_a, pk, enc_rand);
+      crypto_kem_enc_derand(ct, key_a, &pk, enc_rand);
     }
     t1 = get_cyclecounter();
     cycles_enc[i] = t1 - t0;
 
-    // Decapsulation
+    // Deserialize secret key
     for (j = 0; j < NWARMUP; j++) {
-      crypto_kem_dec(key_b, ct, sk);
+      crypto_kem_deserialize_sk(&sk, sks);
     }
     t0 = get_cyclecounter();
     for (j = 0; j < NITERATIONS; j++) {
-      crypto_kem_dec(key_b, ct, sk);
+      crypto_kem_deserialize_sk(&sk, sks);
+    }
+    t1 = get_cyclecounter();
+    cycles_deser_sk[i] = t1 - t0;
+
+
+    // Decapsulation
+    for (j = 0; j < NWARMUP; j++) {
+      crypto_kem_dec(key_b, ct, &sk);
+    }
+    t0 = get_cyclecounter();
+    for (j = 0; j < NITERATIONS; j++) {
+      crypto_kem_dec(key_b, ct, &sk);
     }
     t1 = get_cyclecounter();
     cycles_dec[i] = t1 - t0;
@@ -99,8 +155,19 @@ static int bench(void) {
   qsort(cycles_enc, NTESTS, sizeof(uint64_t), cmp_uint64_t);
   qsort(cycles_dec, NTESTS, sizeof(uint64_t), cmp_uint64_t);
 
+  qsort(cycles_ser_sk, NTESTS, sizeof(uint64_t), cmp_uint64_t);
+  qsort(cycles_ser_pk, NTESTS, sizeof(uint64_t), cmp_uint64_t);
+  qsort(cycles_deser_sk, NTESTS, sizeof(uint64_t), cmp_uint64_t);
+  qsort(cycles_deser_pk, NTESTS, sizeof(uint64_t), cmp_uint64_t);
+
   print_median("keypair", cycles_kg);
+  print_median("serialize_pk", cycles_ser_pk);
+  print_median("serialize_sk", cycles_ser_sk);
+
+  print_median("deserialize_pk", cycles_deser_pk);
   print_median("encaps", cycles_enc);
+
+  print_median("deserialize_sk", cycles_deser_sk);
   print_median("decaps", cycles_dec);
 
   printf("\n");
@@ -108,7 +175,13 @@ static int bench(void) {
   print_percentile_legend();
 
   print_percentiles("keypair", cycles_kg);
+  print_percentiles("serialize_pk", cycles_ser_pk);
+  print_percentiles("serialize_sk", cycles_ser_sk);
+
+  print_percentiles("deserialize_pk", cycles_deser_pk);
   print_percentiles("encaps", cycles_enc);
+
+  print_percentiles("deserialize_sk", cycles_deser_sk);
   print_percentiles("decaps", cycles_dec);
 
   return 0;
