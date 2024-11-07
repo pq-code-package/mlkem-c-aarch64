b31c48d537ada02af46d19e69c3890ee4b3889d5
github-action-benchmark <github@users.noreply.github.com>
2024-11-07 07:32:59 +0000

    add Intel Xeon 4th gen (c7i) (customSmallerIsBetter) benchmark result for 67058dfc2c58bd95cbb3bf9513a05731a6c2fc6f

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1730964629004,
+  "lastUpdate": 1730964779893,
   "repoUrl": "https://github.com/pq-code-package/mlkem-c-aarch64",
   "entries": {
     "Arm Cortex-A72 (Raspberry Pi 4) benchmarks": [
@@ -61349,6 +61349,75 @@ window.BENCHMARK_DATA = {
           {
             "name": "ML-KEM-1024 decaps",
             "value": 89043,
+            "unit": "cycles"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "beckphan@amazon.co.uk",
+            "name": "Hanno Becker",
+            "username": "hanno-becker"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "67058dfc2c58bd95cbb3bf9513a05731a6c2fc6f",
+          "message": "Merge pull request #357 from pq-code-package/cbmc_proof_times_ci\n\nCI: Log CBMC proof times",
+          "timestamp": "2024-11-07T07:28:35Z",
+          "tree_id": "448ebb69dc7bfc21de931310493230f48467925c",
+          "url": "https://github.com/pq-code-package/mlkem-c-aarch64/commit/67058dfc2c58bd95cbb3bf9513a05731a6c2fc6f"
+        },
+        "date": 1730964775672,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "ML-KEM-512 keypair",
+            "value": 25583,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-512 encaps",
+            "value": 33718,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-512 decaps",
+            "value": 43055,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-768 keypair",
+            "value": 43773,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-768 encaps",
+            "value": 48941,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-768 decaps",
+            "value": 62825,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-1024 keypair",
+            "value": 63486,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-1024 encaps",
+            "value": 71733,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-1024 decaps",
+            "value": 89032,
             "unit": "cycles"
           }
         ]
