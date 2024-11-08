b33811da0c7e9508327ad11f3af9d0693329039f
github-action-benchmark <github@users.noreply.github.com>
2024-11-08 07:48:19 +0000

    add AMD EPYC 4th gen (c7a) (customSmallerIsBetter) benchmark result for 4fe39f5082580106057d2c474218b0c11a040e1d

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1731052081087,
+  "lastUpdate": 1731052098959,
   "repoUrl": "https://github.com/pq-code-package/mlkem-native",
   "entries": {
     "Arm Cortex-A72 (Raspberry Pi 4) benchmarks": [
@@ -78392,6 +78392,75 @@ window.BENCHMARK_DATA = {
           {
             "name": "ML-KEM-1024 decaps",
             "value": 90341,
+            "unit": "cycles"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "beckphan@amazon.co.uk",
+            "name": "Hanno Becker",
+            "username": "hanno-becker"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "4fe39f5082580106057d2c474218b0c11a040e1d",
+          "message": "Merge pull request #354 from pq-code-package/keccakstate\n\nFIPS202: Add _ctx_release() functions to support 3rd party FIPS202 implementations using dynamically allocated state",
+          "timestamp": "2024-11-08T07:43:43Z",
+          "tree_id": "19022c8739f40705710ae9f2c6f345f7b78f2fb0",
+          "url": "https://github.com/pq-code-package/mlkem-native/commit/4fe39f5082580106057d2c474218b0c11a040e1d"
+        },
+        "date": 1731052094839,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "ML-KEM-512 keypair",
+            "value": 24748,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-512 encaps",
+            "value": 32172,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-512 decaps",
+            "value": 41598,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-768 keypair",
+            "value": 43543,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-768 encaps",
+            "value": 48383,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-768 decaps",
+            "value": 62244,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-1024 keypair",
+            "value": 63013,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-1024 encaps",
+            "value": 70361,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-1024 decaps",
+            "value": 89791,
             "unit": "cycles"
           }
         ]
