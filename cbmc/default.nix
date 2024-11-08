d0f50f7ed49ab473bca12d02bd7c561fce18abf2
Thing-han, Lim <15379156+potsrevennil@users.noreply.github.com>
2024-11-08 13:54:02 +0800

    build: Update cbmc to 6.4.0
    Resolve #374
    
    Signed-off-by: Thing-han, Lim <15379156+potsrevennil@users.noreply.github.com>

--- cbmc/default.nix
+++ cbmc/default.nix
@@ -6,6 +6,7 @@
 , bitwuzla
 , ninja
 , cadical
+, libgcc
 }:
 
 buildEnv {
@@ -13,16 +14,17 @@ buildEnv {
   paths =
     builtins.attrValues {
       cbmc = cbmc.overrideAttrs (old: rec {
-        version = "6.3.1"; # remember to adjust this in ../flake.nix too
+        version = "6.4.0"; # remember to adjust this in ../flake.nix too
         src = fetchFromGitHub {
           owner = "diffblue";
           repo = old.pname;
           rev = "${old.pname}-${version}";
-          hash = "sha256-y3avPsVxtxSV+WB8TBbvnaNZ4WZltGRTcD+GPwTlp2E=";
+          hash = "sha256-PZZnseOE3nodE0zwyG+82gm55BO4rsCcP4T+fZq7L6I=";
         };
         patches = [
           ./0001-Do-not-download-sources-in-cmake.patch
         ];
+        nativeBuildInputs = old.nativeBuildInputs ++ [ libgcc ];
       });
       litani = callPackage ./litani.nix { }; # 1.29.0
       cbmc-viewer = callPackage ./cbmc-viewer.nix { }; # 3.9
