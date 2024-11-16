fb31c440c4e98457c90920ae280011dbbc67ac5c
github-action-benchmark <github@users.noreply.github.com>
2024-11-16 16:02:25 +0000

    add Graviton4 (customSmallerIsBetter) benchmark result for 4e62a9eb3740a619daacb9f715f223c25500c83d

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1731772929381,
+  "lastUpdate": 1731772945275,
   "repoUrl": "https://github.com/pq-code-package/mlkem-native",
   "entries": {
     "Arm Cortex-A72 (Raspberry Pi 4) benchmarks": [
@@ -70319,6 +70319,75 @@ window.BENCHMARK_DATA = {
           {
             "name": "ML-KEM-1024 decaps",
             "value": 65512,
+            "unit": "cycles"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "beckphan@amazon.co.uk",
+            "name": "Hanno Becker",
+            "username": "hanno-becker"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "4e62a9eb3740a619daacb9f715f223c25500c83d",
+          "message": "Merge pull request #414 from pq-code-package/gh_runner_cleanup\n\nCI: Make EC2 instance-bringup job non-cancellable",
+          "timestamp": "2024-11-16T15:57:19Z",
+          "tree_id": "9217bc16926d5bda8e60812558ec6878fe97c6cf",
+          "url": "https://github.com/pq-code-package/mlkem-native/commit/4e62a9eb3740a619daacb9f715f223c25500c83d"
+        },
+        "date": 1731772938233,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "ML-KEM-512 keypair",
+            "value": 18723,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-512 encaps",
+            "value": 23589,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-512 decaps",
+            "value": 30550,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-768 keypair",
+            "value": 31555,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-768 encaps",
+            "value": 35540,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-768 decaps",
+            "value": 45365,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-1024 keypair",
+            "value": 45664,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-1024 encaps",
+            "value": 52083,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-1024 decaps",
+            "value": 65516,
             "unit": "cycles"
           }
         ]
