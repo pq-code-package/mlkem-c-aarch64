47700d6b8140621bdc4a78b356ed522408be5140
github-action-benchmark <github@users.noreply.github.com>
2024-11-18 03:28:54 +0000

    add Intel Xeon 4th gen (c7i) (customSmallerIsBetter) benchmark result for d30bc18abfb4ea8d6dad6d78544e903b9bd985b0

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1731900453410,
+  "lastUpdate": 1731900534839,
   "repoUrl": "https://github.com/pq-code-package/mlkem-native",
   "entries": {
     "Arm Cortex-A72 (Raspberry Pi 4) benchmarks": [
@@ -79119,6 +79119,75 @@ window.BENCHMARK_DATA = {
           {
             "name": "ML-KEM-1024 decaps",
             "value": 57944,
+            "unit": "cycles"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "matthias@kannwischer.eu",
+            "name": "Matthias J. Kannwischer",
+            "username": "mkannwischer"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "d30bc18abfb4ea8d6dad6d78544e903b9bd985b0",
+          "message": "Merge pull request #421 from pq-code-package/bench_ec2_pmu\n\nBench: Use PMU instead of PERF for x86_64 benchmarking",
+          "timestamp": "2024-11-18T11:25:33+08:00",
+          "tree_id": "31f9fd7451a73a537b703615070bf355e8654b2f",
+          "url": "https://github.com/pq-code-package/mlkem-native/commit/d30bc18abfb4ea8d6dad6d78544e903b9bd985b0"
+        },
+        "date": 1731900527330,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "ML-KEM-512 keypair",
+            "value": 16342,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-512 encaps",
+            "value": 21538,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-512 decaps",
+            "value": 28340,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-768 keypair",
+            "value": 28345,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-768 encaps",
+            "value": 30636,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-768 decaps",
+            "value": 40312,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-1024 keypair",
+            "value": 38629,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-1024 encaps",
+            "value": 42729,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-1024 decaps",
+            "value": 56289,
             "unit": "cycles"
           }
         ]
