0d582db1d714cd92a081d65fc5fd7f43a5f43690
github-action-benchmark <github@users.noreply.github.com>
2024-11-22 10:55:47 +0000

    add Graviton2 (customSmallerIsBetter) benchmark result for 0afbf527184d578167f76dfb6a446c157ebd6962

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1732272939011,
+  "lastUpdate": 1732272947314,
   "repoUrl": "https://github.com/pq-code-package/mlkem-native",
   "entries": {
     "Arm Cortex-A72 (Raspberry Pi 4) benchmarks": [
@@ -55788,6 +55788,75 @@ window.BENCHMARK_DATA = {
           {
             "name": "ML-KEM-1024 decaps",
             "value": 103213,
+            "unit": "cycles"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "beckphan@amazon.co.uk",
+            "name": "Hanno Becker",
+            "username": "hanno-becker"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "0afbf527184d578167f76dfb6a446c157ebd6962",
+          "message": "Merge pull request #448 from pq-code-package/nix-cc\n\nProperly set $CC in nix shell for different architecture",
+          "timestamp": "2024-11-22T10:48:07Z",
+          "tree_id": "9ada86d7edd875a70266535629041a852af11d5b",
+          "url": "https://github.com/pq-code-package/mlkem-native/commit/0afbf527184d578167f76dfb6a446c157ebd6962"
+        },
+        "date": 1732272938475,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "ML-KEM-512 keypair",
+            "value": 29159,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-512 encaps",
+            "value": 37059,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-512 decaps",
+            "value": 47752,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-768 keypair",
+            "value": 49289,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-768 encaps",
+            "value": 57278,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-768 decaps",
+            "value": 72372,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-1024 keypair",
+            "value": 72230,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-1024 encaps",
+            "value": 82807,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-1024 decaps",
+            "value": 103132,
             "unit": "cycles"
           }
         ]
