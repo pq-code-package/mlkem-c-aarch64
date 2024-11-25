eecd9bbd1872d920e2bf4ff49d46ff722bc4ab66
github-action-benchmark <github@users.noreply.github.com>
2024-11-25 13:51:52 +0000

    add Graviton3 (no-opt) (customSmallerIsBetter) benchmark result for 348c5a08c87059970de5f4c2360eec3131ee734e

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1732542672836,
+  "lastUpdate": 1732542711898,
   "repoUrl": "https://github.com/pq-code-package/mlkem-native",
   "entries": {
     "Arm Cortex-A72 (Raspberry Pi 4) benchmarks": [
@@ -108607,6 +108607,75 @@ window.BENCHMARK_DATA = {
           {
             "name": "ML-KEM-1024 decaps",
             "value": 166825,
+            "unit": "cycles"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "beckphan@amazon.co.uk",
+            "name": "Hanno Becker",
+            "username": "hanno-becker"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "348c5a08c87059970de5f4c2360eec3131ee734e",
+          "message": "Merge pull request #453 from pq-code-package/autogen\n\nAuto-generate zeta table for NTT/invNTT",
+          "timestamp": "2024-11-25T13:42:51Z",
+          "tree_id": "aa999ff19dca4dab075a19628537a277c62de3d2",
+          "url": "https://github.com/pq-code-package/mlkem-native/commit/348c5a08c87059970de5f4c2360eec3131ee734e"
+        },
+        "date": 1732542711389,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "ML-KEM-512 keypair",
+            "value": 46429,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-512 encaps",
+            "value": 58784,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-512 decaps",
+            "value": 77964,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-768 keypair",
+            "value": 76373,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-768 encaps",
+            "value": 92511,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-768 decaps",
+            "value": 118127,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-1024 keypair",
+            "value": 113096,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-1024 encaps",
+            "value": 134127,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-1024 decaps",
+            "value": 166806,
             "unit": "cycles"
           }
         ]
