61fb98cc2b2013a4eda9d7b47a95c9734123989c
github-action-benchmark <github@users.noreply.github.com>
2024-12-04 08:36:36 +0000

    add AMD EPYC 4th gen (c7a) (customSmallerIsBetter) benchmark result for 839c588b2473da251936df87898f7dfedd68d873

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1733301380019,
+  "lastUpdate": 1733301396292,
   "repoUrl": "https://github.com/pq-code-package/mlkem-native",
   "entries": {
     "Arm Cortex-A72 (Raspberry Pi 4) benchmarks": [
@@ -113034,6 +113034,75 @@ window.BENCHMARK_DATA = {
           {
             "name": "ML-KEM-1024 decaps",
             "value": 54664,
+            "unit": "cycles"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "matthias@kannwischer.eu",
+            "name": "Matthias J. Kannwischer",
+            "username": "mkannwischer"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "839c588b2473da251936df87898f7dfedd68d873",
+          "message": "Merge pull request #501 from pq-code-package/fix-py-lint\n\nFix python files detection in format and lint scripts",
+          "timestamp": "2024-12-04T16:29:16+08:00",
+          "tree_id": "d5f77551296d36f34ecaaa22d57d5aed76ac36c5",
+          "url": "https://github.com/pq-code-package/mlkem-native/commit/839c588b2473da251936df87898f7dfedd68d873"
+        },
+        "date": 1733301384169,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "ML-KEM-512 keypair",
+            "value": 15091,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-512 encaps",
+            "value": 19753,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-512 decaps",
+            "value": 26393,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-768 keypair",
+            "value": 25587,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-768 encaps",
+            "value": 28188,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-768 decaps",
+            "value": 38212,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-1024 keypair",
+            "value": 35539,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-1024 encaps",
+            "value": 40683,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-1024 decaps",
+            "value": 54463,
             "unit": "cycles"
           }
         ]
