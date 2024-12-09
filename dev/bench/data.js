438456eb390b2cf6e62bcbcd1027e8d324fd77fc
github-action-benchmark <github@users.noreply.github.com>
2024-12-09 11:43:19 +0000

    add Graviton2 (no-opt) (customSmallerIsBetter) benchmark result for 27faf17eaba215a7e93d2fcc48fb95df8e57b3f0

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1733744516107,
+  "lastUpdate": 1733744599614,
   "repoUrl": "https://github.com/pq-code-package/mlkem-native",
   "entries": {
     "Arm Cortex-A72 (Raspberry Pi 4) benchmarks": [
@@ -133502,6 +133502,75 @@ window.BENCHMARK_DATA = {
           {
             "name": "ML-KEM-1024 decaps",
             "value": 254660,
+            "unit": "cycles"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "beckphan@amazon.co.uk",
+            "name": "Hanno Becker",
+            "username": "hanno-becker"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "27faf17eaba215a7e93d2fcc48fb95df8e57b3f0",
+          "message": "Merge pull request #504 from pq-code-package/comments\n\nMove all function signature documentation to header files",
+          "timestamp": "2024-12-09T11:33:04Z",
+          "tree_id": "925a07332342eda26984acb5793a28e4bba3146f",
+          "url": "https://github.com/pq-code-package/mlkem-native/commit/27faf17eaba215a7e93d2fcc48fb95df8e57b3f0"
+        },
+        "date": 1733744598336,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "ML-KEM-512 keypair",
+            "value": 71123,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-512 encaps",
+            "value": 87832,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-512 decaps",
+            "value": 118151,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-768 keypair",
+            "value": 117619,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-768 encaps",
+            "value": 139361,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-768 decaps",
+            "value": 180090,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-1024 keypair",
+            "value": 175150,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-1024 encaps",
+            "value": 202627,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-KEM-1024 decaps",
+            "value": 254563,
             "unit": "cycles"
           }
         ]
