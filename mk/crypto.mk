1c0ac3b856d8ebb22a74e6120c3453e3387da9f7
Hanno Becker <beckphan@amazon.co.uk>
2024-12-13 07:03:46 +0000

    Build: Don't build a libfips202.a
    The FIPS-202 code in mlkem-native is not meant as a standalone library;
    instead, it only provides the minimal functionality needed to implement
    ML-KEM. As such, it should not be generated as a separate library, but
    directly linked into the libmlkem{XXX}.a.
    
    This commit adjusts the build accordingly.
    
    Signed-off-by: Hanno Becker <beckphan@amazon.co.uk>

--- mk/crypto.mk
+++ mk/crypto.mk
@@ -5,18 +5,7 @@ ifeq ($(OPT),1)
 	FIPS202_SRCS += $(wildcard fips202/native/aarch64/*.S) $(wildcard fips202/native/x86_64/xkcp/*.c)
 endif
 
-$(BUILD_DIR)/libfips202.a: $(call OBJS, $(FIPS202_SRCS))
 $(BUILD_DIR)/libmlkem.a: $(call OBJS, $(FIPS202_SRCS))
-
-# all lib<scheme>.a depends on libfips202.a
-define ADD_FIPS202
-$(BUILD_DIR)/lib$(1).a: LDLIBS += -lfips202
-# NOTE:
-# - Merging multiple .a files with ar is more complex than building a single library directly from all the object files (.o). Hence, all .o files are added as dependencies here.
-
-$(BUILD_DIR)/lib$(1).a: $(BUILD_DIR)/libfips202.a $(call OBJS, $(FIPS202_SRCS))
-endef
-
-$(foreach scheme,mlkem512 mlkem768 mlkem1024, \
-	$(eval $(call ADD_FIPS202,$(scheme))) \
-)
+$(BUILD_DIR)/libmlkem512.a: $(call OBJS, $(FIPS202_SRCS))
+$(BUILD_DIR)/libmlkem768.a: $(call OBJS, $(FIPS202_SRCS))
+$(BUILD_DIR)/libmlkem1024.a: $(call OBJS, $(FIPS202_SRCS))
